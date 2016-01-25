1e8e2933dc8a1204566b332c19c689300a29d4c8
MGlauer <martinglauer89@googlemail.com>
2016-01-25 15:48:32 +0100

    Table overview and pending data promt
    Added an overview of all tables and created menu entry
    Users are promted if leaving site with pending data. closes #8

--- dataedit/urls.py
+++ dataedit/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DataView.as_view(), name='index'),
+    url(r'^$', views.listtables, name='index'),
     url(r'^edit', views.DataInputView.as_view(), name='input'),
     url(r'^delete', views.delete, name='input'),
+    url(r'^drop', views.dropSessionData, name='input'),
     url(r'^view/(?P<schema>.+)/(?P<table>.+)', views.DataView.as_view(), name='input'),
     url(r'^upload', views.DataUploadView.as_view(), name='input'),
     url(r'^map', views.DataMapView.as_view(), name='input'),
--- oeplatform/settings.py
+++ oeplatform/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sessions.backends.signed_cookies',
+    'widget_tweaks',
     'bootstrap3',
     'base',
 	'dataedit'
