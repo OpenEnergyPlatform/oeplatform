f6bb55229966c67c5817e672ad0118e175a988f8
MGlauer <martinglauer89@googlemail.com>
2016-02-04 09:47:31 +0100

    Added proper link hierarchy for databases, schemas and tables

--- dataedit/urls.py
+++ dataedit/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url
 
 from . import views
 
+pgsql_qualifier = r"[\w\d_]+"
+
 urlpatterns = [
     url(r'^$', views.listtables, name='index'),
     url(r'^edit', views.DataInputView.as_view(), name='input'),
     url(r'^delete', views.delete, name='input'),
     url(r'^drop', views.dropSessionData, name='input'),
-    url(r'^view/(?P<schema>.+)/(?P<table>.+)', views.DataView.as_view(), name='input'),
+    url(r'^view/$', views.listdbs, name='index'),
+    url(r'^view/(?P<db>{qual})$'.format(qual=pgsql_qualifier) , views.listschemas, name='input'),
+    url(r'^view/(?P<db>{qual})/(?P<schema>{qual})$'.format(qual=pgsql_qualifier), views.listtables, name='input'),
+    url(r'^view/(?P<db>{qual})/(?P<schema>{qual})/(?P<table>{qual})'.format(qual=pgsql_qualifier), views.DataView.as_view(), name='input'),
     url(r'^upload', views.DataUploadView.as_view(), name='input'),
     url(r'^map', views.DataMapView.as_view(), name='input'),
     url(r'^confirm', views.confirm, name='input'),
+    url(r'^commit', views.commit, name='input'),
 ]
