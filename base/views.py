476b97cac9d134cad0a810f4eb9076da4953348b
MGlauer <martinglauer89@googlemail.com>
2016-02-25 13:33:13 +0100

    Remove obsolete init

--- base/views.py
+++ base/views.py
@@ -4,9 +4,6 @@ from django.views.generic import View
 # Create your views here.
 
 class Welcome(View):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args,*kwargs)
-
-    
+   
     def get(self, request):
         return render(request,'base/index.html',{}) 
