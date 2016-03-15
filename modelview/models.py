be10d6741290f8e76f8088abcbace3527b2919d7
MGlauer <martinglauer89@googlemail.com>
2016-03-15 14:43:40 +0100

    Added other textfield to "approach to uncertainty"
    closes #32

--- modelview/models.py
+++ modelview/models.py
@@ -194,6 +194,7 @@ class Energymodel(BasicFactsheet):
     uncertainty_deterministic = BooleanField(default=False,verbose_name='Deterministic') 
     uncertainty_Stochastic = BooleanField(default=False,verbose_name='Stochastic')
     uncertainty_Other = BooleanField(default=False,verbose_name='Other')
+    uncertainty_Other_text = BooleanField(default=False,verbose_name='Other')
 
     montecarlo = BooleanField(default=False,verbose_name='Suited for many scenarios / monte-carlo') 
 
