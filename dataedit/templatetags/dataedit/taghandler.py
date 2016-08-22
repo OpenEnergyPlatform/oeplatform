71166b445eb5b4aec407b743f8167842e21ed28f
MGlauer <martinglauer89@googlemail.com>
2016-08-22 10:47:52 +0200

    Remove unnecessary variable assignment and print

--- dataedit/templatetags/dataedit/taghandler.py
+++ dataedit/templatetags/dataedit/taghandler.py
@@ -9,12 +9,12 @@ register = template.Library()
 def get_tags():
     return models.Tag.objects.all()[:10]
 
+
 @register.simple_tag()
 def readable_text_color(color_hex):
-    r,g,b = webcolors.hex_to_rgb(color_hex)
-    L = 0.2126 * r + 0.7152 * g+ 0.0722 * b
-    print((r,g,b), L, 0.279*255)
-    if L < 0.279*255:
+    r, g, b = webcolors.hex_to_rgb(color_hex)
+    # Calculate brightness of the background and compare to threshold
+    if 0.2126 * r + 0.7152 * g+ 0.0722 * b < 0.279*255:
         return "#FFFFFF"
     else:
         return "#000000"
