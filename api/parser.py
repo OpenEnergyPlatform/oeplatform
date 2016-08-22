49e713c646fa793ffe0009f8dc4cb09b65ab1e27
MGlauer <martinglauer89@googlemail.com>
2016-08-22 18:39:19 +0200

    Refactor parser

--- api/parser.py
+++ api/parser.py
@@ -5,6 +5,7 @@ import re
 
 pgsql_qualifier = re.compile(r"^[\w\d_\.]+$")
 
+
 def is_pg_qual(x):
     return pgsql_qualifier.search(x)
 
@@ -14,16 +15,19 @@ class ValidationError(Exception):
         self.message = message
         self.value = value
 
+
 def read_bool(s):
-    if  s.lower() in ["true","false"]:
+    if s.lower() in ["true", "false"]:
         return s.lower() == "true"
     else:
         raise ValidationError("Invalid value in binary field", s)
 
+
 def read_pgid(s):
-     if is_pg_qual(s):
+    if is_pg_qual(s):
         return s
-     raise ValidationError("Invalid identifier", s)
+    raise ValidationError("Invalid identifier", s)
+
 
 def parse_select(d):
     """
@@ -34,7 +38,7 @@ def parse_select(d):
             [ WITH [ RECURSIVE ] with_query [, ...] ]
             [ WINDOW window_name AS ( window_definition ) [, ...] ]
             [ FOR { UPDATE | NO KEY UPDATE | SHARE | KEY SHARE } [ OF table_name [, ...] ] [ NOWAIT ] [...] ]
-    """    
+    """
     s = 'SELECT'
     # [ ALL | DISTINCT [ ON ( expression [, ...] ) ] ]
     if 'all' in d and read_bool(d['all']):
@@ -42,8 +46,7 @@ def parse_select(d):
     elif 'distinct' in d:
         s += ' DISTINCT'
         if d['distinct']:
-            s += ' (' + ', '.join(map(parse_expression,d)) + ') '
-    
+            s += ' (' + ', '.join(map(parse_expression, d)) + ') '
 
     L = []
 
@@ -57,52 +60,52 @@ def parse_select(d):
     else:
         s += ' * '
 
-
     # [ FROM from_item [, ...] ]
     if 'from' in d:
         s += ' FROM ' + ', '.join(parse_from_item(f) for f in d['from'])
-    
+
     # [ WHERE condition ]
     if 'where' in d:
-        s+= ' WHERE ' + parse_condition(d['where'])
-        
+        s += ' WHERE ' + parse_condition(d['where'])
+
     if 'group_by' in d:
-        s += ' GROUP BY ' + ', '.join(parse_expression(f) for f in d['group_by'])
-        
+        s += ' GROUP BY ' + ', '.join(
+            parse_expression(f) for f in d['group_by'])
+
     if 'having' in d:
         s += ' HAVING ' + ', '.join(parse_condition(f) for f in d['having'])
-            
+
     if 'select' in d:
         sel = d['select']
-        if sel['type'].lower() in ['union','intersect','except']:
+        if sel['type'].lower() in ['union', 'intersect', 'except']:
             s += ' ' + sel['type']
         else:
             raise ValidationError('UNION/INTERSECT/EXCEPT expected')
         if 'all' in sel and read_bool(sel['all']):
             s += ' ALL '
         elif 'distinct' in sel and read_bool(sel['distinct']):
-            s += ' DISTINCT ' 
-        s += parse_select(sel['select'])   
-        
+            s += ' DISTINCT '
+        s += parse_select(sel['select'])
+
     if 'order_by' in d:
         L = []
         for ob in d['order_by']:
             ss = ''
             ss += ' ORDER BY ' + parse_expression(ob)
             if 'ordering' in ob:
-                if ob['ordering'] in ['ASC','DESC']:
+                if ob['ordering'] in ['ASC', 'DESC']:
                     ss += ' ' + ob['ordering']
                 else:
                     ss += parse_operator(ob['ordering'])
             if 'nulls' in ob:
                 ss += ' NULLS '
-                if ob['nulls'].lower() in ['first','last']:
+                if ob['nulls'].lower() in ['first', 'last']:
                     ss += ob['nulls']
                 else:
-                    raise ValidationError('Invalid NULLS option')  
+                    raise ValidationError('Invalid NULLS option')
             L.append(ss)
         s += ', '.join(L)
-    
+
     if 'limit' in d:
         s += ' LIMIT'
         if isinstance(d['limit'], str) and d['limit'].lower() == 'all':
@@ -111,17 +114,18 @@ def parse_select(d):
             s += ' ' + str(d['limit'])
         else:
             raise ValidationError('Invalid LIMIT (expected ALL or a digit)')
-            
-    if 'offset' in d and (isinstance(d['offset'], int) or d['offset'].is_digit()):
+
+    if 'offset' in d and (
+                isinstance(d['offset'], int) or d['offset'].is_digit()):
         s += ' OFFSET {} ROWS'.format(d['offset'])
-        
+
     if 'fetch' in d and d['fetch'].is_digit():
         s += ' FETCH NEXT {} ROWS ONLY'.format(d['fetch'])
 
     print(s)
-    return s                      
-        
-        
+    return s
+
+
 def parse_from_item(d):
     """
         Defintion of a from_item according to 
@@ -137,50 +141,52 @@ def parse_from_item(d):
     # TODO: If 'type' is not set assume just a table name is present
     if d['type'] == 'table':
         s = 'only ' if d.pop('only', False) else ''
-        schema = read_pgid(d.pop('schema',''))
+        schema = read_pgid(d.pop('schema', ''))
         if schema:
-            s += schema+"."
+            s += schema + "."
         s += read_pgid(d['table'])
         if 'star' in d and read_bool(d['star']):
             s += ' * '
         if 'alias' in d and read_bool(d['alias']):
             s += ' AS ' + read_pgid(d['alias'])
             s += ' (' + \
-                ', '.join(map(read_pgid,d['alias'].pop('column_aliases',[]))) + \
-                ')'
-                        
+                 ', '.join(
+                     map(read_pgid, d['alias'].pop('column_aliases', []))) + \
+                 ')'
+
     elif d['type'] == 'select':
         s = 'LATERAL ' if read_bool(d['lateral']) else ''
-        s += '(' + parse_select(d['select']) +') '
+        s += '(' + parse_select(d['select']) + ') '
         if 'alias' in d and read_bool(d['alias']):
             s += ' AS ' + read_pgid(d['alias'])
             s += ' (' + \
-                ', '.join(map(read_pgid,d['alias'].pop('column_aliases',[]))) + \
-                ')'
-                
+                 ', '.join(
+                     map(read_pgid, d['alias'].pop('column_aliases', []))) + \
+                 ')'
+
     elif d['type'] == 'join':
         s = parse_from_item(d['left'])
         if 'natural' in d and read_bool(d['natural']):
             s += ' NATURAL'
         if 'join_type' in d:
-            if d['join_type'].lower() in [  'join', 'inner join', 'left join',
-                                            'left outer join', 'right join',
-                                            'right outer join', 'full join',
-                                            'full inner join', 'cross join']:
-               s +=  ' ' + d['join_type']
+            if d['join_type'].lower() in ['join', 'inner join', 'left join',
+                                          'left outer join', 'right join',
+                                          'right outer join', 'full join',
+                                          'full inner join', 'cross join']:
+                s += ' ' + d['join_type']
             else:
                 raise ValidationError('Invalid join type')
         else:
             s += ' JOIN '
-        
+
         s += ' ' + parse_from_item(d['right'])
-        
+
         if 'on' in d:
             s += ' ON ' + parse_condition(d['on'])
         elif 'using' in d:
-            s += ' USING (' + ', '.join(map(read_pgid,d['using'])) + ')'
+            s += ' USING (' + ', '.join(map(read_pgid, d['using'])) + ')'
     return s
-           
+
 
 def parse_expression(d):
     # TODO: Implement
@@ -196,6 +202,7 @@ def parse_expression(d):
 
     raise NotImplementedError()
 
+
 def parse_condition(dl):
     # TODO: Implement
     if type(dl) == dict:
@@ -203,56 +210,64 @@ def parse_condition(dl):
     conditionlist = []
     for d in dl:
         if d['type'] == 'operator_binary':
-            conditionlist.append("%s %s %s" % (parse_expression(d['left']), d['operator'], parse_expression(d['right'])))
-    
+            conditionlist.append("%s %s %s" % (
+                parse_expression(d['left']), d['operator'],
+                parse_expression(d['right'])))
+
     return " " + " AND ".join(conditionlist)
-    
+
+
 def parse_operator(d):
     # TODO: Implement
     if d['operator'] == 'as':
         return parse_expression(d['labeled']) + " AS " + d['label_name']
     if d['operator'] == 'function':
-        assert(read_pgid(d['function']))
-        return '{f}({ops})'.format(f=d['function'], ops=', '.join(map(parse_expression,d['operands'])))
+        assert (read_pgid(d['function']))
+        return '{f}({ops})'.format(f=d['function'], ops=', '.join(
+            map(parse_expression, d['operands'])))
     return d
 
+
 def parse_function(d):
-    assert(read_pgid(d['function']))
-    return '{f}({ops})'.format(f=d['function'], ops=', '.join(map(parse_expression,d['operands'])))
-    
+    assert (read_pgid(d['function']))
+    return '{f}({ops})'.format(f=d['function'], ops=', '.join(
+        map(parse_expression, d['operands'])))
+
+
 def cadd(d, key, string=None):
-    if not string: 
+    if not string:
         string = key.upper() + ' '
-    if d.pop(key,None):
+    if d.pop(key, None):
         return string
     else:
         return ''
-        
+
+
 def parse_create_table(d):
     s = 'CREATE '
-    if d.pop('global',None):
+    if d.pop('global', None):
         s += 'GLOBAL '
-    elif d.pop('local',None):
+    elif d.pop('local', None):
         s += 'LOCAL '
-    
-    s += cadd(d, 'temp') 
-    + cadd(d, 'unlogged')
-    + 'TABLE '
-    + cadd(d,'if_not_exists', 'IF NOT EXISTS ')
-    + read_pgid(d['name'])
-    
+
+    s += (cadd(d, 'temp')
+          + cadd(d, 'unlogged')
+          + 'TABLE '
+          + cadd(d, 'if_not_exists', 'IF NOT EXISTS ')
+          + read_pgid(d['name']))
+
     fieldstrings = []
-    
-    for field in d.pop('fields',[]):
+
+    for field in d.pop('fields', []):
         fs = ''
         if field['type'] == 'column':
             fs += read_pgid(field['name']) + ' '
             fs += read_pgid(field['data_type']) + ' '
-            collate = field.pop('collate',None)
+            collate = field.pop('collate', None)
             if collate:
                 fs += read_pgid(collate)
-            fs += ', '.join([parse_column_constraint(cons) 
-                for cons in field.pop('constraints',[])]) + ' '
+            fs += ', '.join([parse_column_constraint(cons)
+                             for cons in field.pop('constraints', [])]) + ' '
         elif field['type'] == 'table_constraint':
             fs += parse_table_constraint(field)
         elif field['type'] == 'like':
@@ -262,7 +277,8 @@ def parse_create_table(d):
 def parse_column_constraint(d):
     raise NotImplementedError
     # TODO: Implement
-    
+
+
 def parse_table_constraint(d):
     raise NotImplementedError
     # TODO: Implement
