5e3b59f9359910592e39d1af33cec7e8d5f0052f
MGlauer <martinglauer89@googlemail.com>
2017-01-23 12:23:34 +0100

    Add legal and information pages to base app

--- base/views.py
+++ base/views.py
@@ -7,3 +7,7 @@ class Welcome(View):
    
     def get(self, request):
         return render(request,'base/index.html',{}) 
+
+
+def redir(request, target):
+    return render(request, 'base/{target}.html'.format(target=target),{})
