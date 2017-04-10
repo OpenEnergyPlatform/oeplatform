3018646a5f90a11a5de1c46bb0d9a6120b69df82
MGlauer <martinglauer89@gmail.com>
2017-04-10 18:49:34 +0200

    Implement downwards compatibility for initial Metadata version

--- dataedit/views.py
+++ dataedit/views.py
@@ -648,20 +648,54 @@ Metadata handler
 def load_comments(schema, table):
     comment_on_table = actions.get_comment_table(sec.dbname, schema, table)
     columns = actions.analyze_columns(sec.dbname, schema, table)
+    try:
+        if 'error' in comment_on_table:
+            comment_on_table = {'description': [comment_on_table['content']], 'fields': []}
+            commented_cols = []
+        elif 'resources' not in comment_on_table:
+            comment_on_table = {
+                'title': comment_on_table['Name'],
+                'description': "; ".join(comment_on_table['Description']),
+                'language': [],
+                'reference_date': comment_on_table['Reference date'],
+                'sources': [
+                    {'name': x['Name'], 'description': '', 'url': x['URL'], 'license': ' ', 'copyright': ' '} for x in comment_on_table['Source']],
+                'spatial': [
+                    {'extend': x, 'resolution': ''} for x in comment_on_table['Spatial resolution']],
+                'license': [
+                    {'id': '',
+                    'name': x,
+                    'version': '',
+                    'url': '',
+                    'instruction': '',
+                    'copyright': ''} for x in comment_on_table['Licence']],
+                'contributors': [
+                    {'name': x['Name'], 'email': x['Mail'], 'date': x['Date'], 'comment': x['Comment']} for x in comment_on_table['Changes']],
+                'resources': [{
+                    'schema': {
+                        'fields': [
+                            {'name': x['Name'], 'description': x['Description'], 'unit': x['Unit'] } for x in comment_on_table['Column']]},
+                    'meta_version': '1.2' }] }
 
-    if 'error' in comment_on_table or not comment_on_table['resources']:
-        comment_on_table = {'description': [comment_on_table['content']]}
+            comment_on_table['fields'] = comment_on_table['resources'][0]['schema']['fields']
 
-    comment_on_table['fields'] = comment_on_table['resources'][0]['schema'][
-        'fields']
+            commented_cols = [col['name'] for col in comment_on_table['fields']]
+        else:
+            comment_on_table['fields'] = comment_on_table['resources'][0]['schema'][
+                'fields']
 
-    if 'fields' not in comment_on_table['resources'][0]['schema']:
-        comment_on_table['fields'] = []
-    else:
-        comment_on_table['fields'] = comment_on_table['resources'][0]['schema'][
-            'fields']
+            if 'fields' not in comment_on_table['resources'][0]['schema']:
+                comment_on_table['fields'] = []
+            else:
+                comment_on_table['fields'] = comment_on_table['resources'][0]['schema'][
+                    'fields']
 
-    commented_cols = [col['name'] for col in comment_on_table['fields']]
+            commented_cols = [col['name'] for col in comment_on_table['fields']]
+    except Exception as e:
+        print(e)
+        comment_on_table = {'description': comment_on_table, 'fields': []}
+        commented_cols = []
+
     for col in columns:
         if not col['id'] in commented_cols:
             comment_on_table['fields'].append({
