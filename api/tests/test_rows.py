25047cd00b59fceff39dff1122c095fb0d78e797
MGlauer <martinglauer89@gmail.com>
2017-08-04 19:39:40 +0200

    Implement tests for Rows.get

--- api/tests/test_rows.py
+++ api/tests/test_rows.py
@@ -231,34 +231,6 @@ class TestRowsPost(APITestCase):
 
         assert c_basic_resp.status_code==201, c_basic_resp.json().get('reason','No reason returned')
 
-    def tearDown(self):
-        meta_schema = actions.get_meta_schema_name(self.test_schema)
-        if actions.has_table(
-                dict(table=self.test_table, schema=self.test_schema)):
-            actions.perform_sql(
-                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
-                    schema=meta_schema,
-                    table=actions.get_insert_table_name(self.test_schema,
-                                                        self.test_table)
-                ))
-            actions.perform_sql(
-                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
-                    schema=meta_schema,
-                    table=actions.get_edit_table_name(self.test_schema,
-                                                      self.test_table)
-                ))
-            actions.perform_sql(
-                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
-                    schema=meta_schema,
-                    table=actions.get_delete_table_name(self.test_schema,
-                                                        self.test_table)
-                ))
-            actions.perform_sql(
-                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
-                    schema=self.test_schema,
-                    table=self.test_table
-                ))
-
     def test_simple_post_new(self, rid=1):
         row = {'id': rid, 'name': 'Mary Doe', 'address': "Mary's Street",
                'geom':'POINT(-71.160281 42.258729)'}
@@ -323,3 +295,108 @@ class TestRowsPost(APITestCase):
 
         row['geom'] = wkb.dumps(wkt.loads(row['geom']), hex=True)
         self.assertDictEqualKeywise(response.json(), row)
+
+class TestRowsPost(APITestCase):
+    def setUp(self):
+        self.rows = [{'id': 1, 'name': 'John Doe', 'address': None, 'geom': 'Point(-71.160281 42.258729)'}]
+        self.test_table = 'test_table_rows'
+        self.test_schema = 'schema1'
+        structure_data = {
+            "constraints": [
+                {
+                    "constraint_type": "PRIMARY KEY",
+                    "constraint_parameter": "id",
+                    "reference_table": None,
+                    "reference_column": None
+                }
+            ],
+            "columns": [
+                {
+                    "name": "id",
+                    "data_type": "integer",
+                    "is_nullable": False,
+                    "character_maximum_length": None
+                },
+                {
+                    "name": "name",
+                    "data_type": "character varying",
+                    "is_nullable": True,
+                    "character_maximum_length": 50
+                }, {
+                    "name": "address",
+                    "data_type": "character varying",
+                    "is_nullable": True,
+                    "character_maximum_length": 150
+                }, {
+                    "name": "geom",
+                    "data_type": "geometry (point)",
+                    "is_nullable": True,
+                }
+            ]
+        }
+
+        c_basic_resp = self.__class__.client.put(
+            '/api/v0/schema/{schema}/tables/{table}/'.format(
+                schema=self.test_schema, table=self.test_table),
+            data=json.dumps({'query': structure_data}),
+            HTTP_AUTHORIZATION='Token %s' % self.__class__.token,
+            content_type='application/json')
+
+        assert c_basic_resp.status_code==201, c_basic_resp.json().get('reason','No reason returned')
+
+        self.rows = []
+
+        for i in range(100):
+            row = {'id': i, 'name': 'Mary Doe', 'address': "Mary's Street",
+                   'geom': '0101000000E44A3D0B42CA51C06EC328081E214540'}
+
+            response = self.__class__.client.put(
+                '/api/v0/schema/{schema}/tables/{table}/rows/{rid}'.format(
+                    schema=self.test_schema, table=self.test_table, rid=i),
+                data=json.dumps({'query': row}),
+                HTTP_AUTHORIZATION='Token %s' % self.__class__.token,
+                content_type='application/json')
+
+            assert c_basic_resp.status_code == 201, c_basic_resp.json().get(
+                'reason', 'No reason returned')
+
+            self.rows.append(row)
+
+    def tearDown(self):
+        meta_schema = actions.get_meta_schema_name(self.test_schema)
+        if actions.has_table(
+                dict(table=self.test_table, schema=self.test_schema)):
+            actions.perform_sql(
+                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
+                    schema=meta_schema,
+                    table=actions.get_insert_table_name(self.test_schema,
+                                                        self.test_table)
+                ))
+            actions.perform_sql(
+                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
+                    schema=meta_schema,
+                    table=actions.get_edit_table_name(self.test_schema,
+                                                      self.test_table)
+                ))
+            actions.perform_sql(
+                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
+                    schema=meta_schema,
+                    table=actions.get_delete_table_name(self.test_schema,
+                                                        self.test_table)
+                ))
+            actions.perform_sql(
+                "DROP TABLE IF EXISTS {schema}.{table} CASCADE".format(
+                    schema=self.test_schema,
+                    table=self.test_table
+                ))
+
+    def test_simple_get(self):
+        response = self.__class__.client.get(
+            '/api/v0/schema/{schema}/tables/{table}/rows/'.format(
+                schema=self.test_schema, table=self.test_table))
+
+        self.assertEqual(response.status_code, 200,
+                         response.json())
+
+        for c in zip(response.json(), self.rows):
+            self.assertDictEqualKeywise(*c)
