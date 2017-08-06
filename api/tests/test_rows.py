4087cfd92f79e122c63f94262fdca8c67b86c8ff
MGlauer <martinglauer89@googlemail.com>
2017-08-06 16:31:05 +0200

    Implement test for Rows.get with offset

--- api/tests/test_rows.py
+++ api/tests/test_rows.py
@@ -324,9 +324,11 @@ class TestPost(APITestCase):
         self.assertDictEqualKeywise(response.json(), row)
 
 class TestGet(APITestCase):
-    def setUp(self):
-        self.rows = [{'id': 1, 'name': 'John Doe', 'address': None, 'geom': 'Point(-71.160281 42.258729)'}]
-        self.test_table = 'test_table_rows'
+    @classmethod
+    def setUpClass(cls):
+        super(TestGet, cls).setUpClass()
+        cls.rows = [{'id': 1, 'name': 'John Doe', 'address': None, 'geom': 'Point(-71.160281 42.258729)'}]
+        cls.test_table = 'test_table_rows'
         structure_data = {
             "constraints": [
                 {
@@ -361,34 +363,36 @@ class TestGet(APITestCase):
             ]
         }
 
-        c_basic_resp = self.__class__.client.put(
+        c_basic_resp = cls.client.put(
             '/api/v0/schema/{schema}/tables/{table}/'.format(
-                schema=self.test_schema, table=self.test_table),
+                schema=cls.test_schema, table=cls.test_table),
             data=json.dumps({'query': structure_data}),
-            HTTP_AUTHORIZATION='Token %s' % self.__class__.token,
+            HTTP_AUTHORIZATION='Token %s' % cls.token,
             content_type='application/json')
 
         assert c_basic_resp.status_code==201, c_basic_resp.json()
 
-        self.rows = []
+        cls.rows = []
 
         for i in range(100):
             row = {'id': i, 'name': 'Mary Doe', 'address': "Mary's Street",
                    'geom': '0101000000E44A3D0B42CA51C06EC328081E214540'}
 
-            response = self.__class__.client.put(
+            response = cls.client.put(
                 '/api/v0/schema/{schema}/tables/{table}/rows/{rid}'.format(
-                    schema=self.test_schema, table=self.test_table, rid=i),
+                    schema=cls.test_schema, table=cls.test_table, rid=i),
                 data=json.dumps({'query': row}),
-                HTTP_AUTHORIZATION='Token %s' % self.__class__.token,
+                HTTP_AUTHORIZATION='Token %s' % cls.token,
                 content_type='application/json')
 
             assert c_basic_resp.status_code == 201, c_basic_resp.json().get(
                 'reason', 'No reason returned')
 
-            self.rows.append(row)
+            cls.rows.append(row)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
+        super(TestGet, self).tearDownClass()
         meta_schema = actions.get_meta_schema_name(self.test_schema)
         if actions.has_table(
                 dict(table=self.test_table, schema=self.test_schema)):
@@ -425,4 +429,15 @@ class TestGet(APITestCase):
                          response.json())
 
         for c in zip(response.json(), self.rows):
+            self.assertDictEqualKeywise(*c)
+
+    def test_simple_offset(self):
+        response = self.__class__.client.get(
+            '/api/v0/schema/{schema}/tables/{table}/rows/?offset=50'.format(
+                schema=self.test_schema, table=self.test_table))
+
+        self.assertEqual(response.status_code, 200,
+                         response.json())
+
+        for c in zip(response.json(), self.rows[50:]):
             self.assertDictEqualKeywise(*c)
