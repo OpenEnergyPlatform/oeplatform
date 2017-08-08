121dfbfb970fac362b7bf26fe523b9119019d513
MGlauer <martinglauer89@gmail.com>
2017-08-08 14:42:51 +0200

    Use SQL-Alchemy for data_select

--- api/parser.py
+++ api/parser.py
@@ -4,7 +4,7 @@
 import decimal
 import re
 from datetime import datetime
-from sqlalchemy import Table, MetaData, Column
+from sqlalchemy import Table, MetaData, Column, select, column
 from api.error import APIError
 from api.connection import _get_engine
 import geoalchemy2  # Although this import seems unused is has to be here
@@ -121,87 +121,61 @@ def parse_select(d):
             [ WINDOW window_name AS ( window_definition ) [, ...] ]
             [ FOR { UPDATE | NO KEY UPDATE | SHARE | KEY SHARE } [ OF table_name [, ...] ] [ NOWAIT ] [...] ]
     """
-    s = 'SELECT'
-    # [ ALL | DISTINCT [ ON ( expression [, ...] ) ] ]
-    if 'all' in d and read_bool(d['all']):
-        s += ' ALL'
-    elif 'distinct' in d:
-        s += ' DISTINCT'
+    distinct = d.get('distinct', False)
 
-    L = []
+    L = None
 
     if 'fields' in d and d['fields']:
+        L = []
         for field in d['fields']:
-            ss = parse_expression(field)
-            if 'as' in field and '_count' not in d:
-                ss += ' AS ' + read_pgid(field['as'])
-            L.append(ss)
-        s += ' ' + ', '.join(L)
-    else:
-        s += ' * '
-
-    # [ FROM from_item [, ...] ]
-    if 'from' in d:
-        s += ' FROM ' + ', '.join(parse_from_item(f) for f in d['from'])
+            col = parse_expression(field)
+            if 'as' in field:
+                col.label(read_pgid(field['as']))
+            L.append(col)
+    from_clause = parse_from_item(d['from'])
+    if not L:
+        L = '*'
+    query = select(columns=L, distinct=distinct, from_obj=from_clause)
 
     # [ WHERE condition ]
     if d.get('where', False):
-        s += ' WHERE ' + parse_condition(d['where'])
+        query = query.where(parse_condition(d['where']))
 
     if 'group_by' in d:
-        s += ' GROUP BY ' + ', '.join(
-            parse_expression(f) for f in d['group_by'])
+        query = query.group_by([parse_expression(f) for f in d['group_by']])
 
     if 'having' in d:
-        s += ' HAVING ' + ', '.join(parse_condition(f) for f in d['having'])
+        query.having([parse_condition(f) for f in d['having']])
 
     if 'select' in d:
-        sel = d['select']
-        if sel['type'].lower() in ['union', 'intersect', 'except']:
-            s += ' ' + sel['type']
-        else:
-            raise APIError('UNION/INTERSECT/EXCEPT expected')
-        if 'all' in sel and read_bool(sel['all']):
-            s += ' ALL '
-        elif 'distinct' in sel and read_bool(sel['distinct']):
-            s += ' DISTINCT '
-        s += parse_select(sel['select'])
-
+        type = d.pop('type')
+        subquery = parse_select(d['select'])
+        if type.lower() == 'union':
+            query.union(subquery)
+        elif type.lower() == 'intersect':
+            query.intersect(subquery)
+        elif type.lower() == 'except':
+            query.except_(subquery)
     if 'order_by' in d:
-        L = []
         for ob in d['order_by']:
-            ss = ''
-            ss += ' ORDER BY ' + parse_expression(ob)
-            if 'ordering' in ob:
-                if ob['ordering'] in ['asc', 'desc']:
-                    ss += ' ' + ob['ordering']
-                else:
-                    ss += parse_operator(ob['ordering'])
-            if 'nulls' in ob:
-                ss += ' NULLS '
-                if ob['nulls'].lower() in ['first', 'last']:
-                    ss += ob['nulls']
-                else:
-                    raise APIError('Invalid NULLS option')
-            L.append(ss)
-        s += ', '.join(L)
+            expr = parse_expression(ob)
+            desc = ob.get('ordering', 'asc').lower() == 'desc'
+            if desc:
+                expr = expr.desc()
+            query = query.order_by(expr)
 
     if 'limit' in d:
-        s += ' LIMIT'
-        if isinstance(d['limit'], str) and d['limit'].lower() == 'all':
-            s += ' ALL'
-        elif isinstance(d['limit'], int) or d['limit'].is_digit():
-            s += ' ' + str(d['limit'])
+        if isinstance(d['limit'], int) or d['limit'].is_digit():
+            query = query.limit(int(d['limit']))
         else:
-            raise APIError('Invalid LIMIT (expected ALL or a digit)')
+            raise APIError('Invalid LIMIT: Expected a digit')
 
-    if 'offset' in d and (
-                isinstance(d['offset'], int) or d['offset'].is_digit()):
-        s += ' OFFSET {} ROWS'.format(d['offset'])
-
-    if 'fetch' in d and d['fetch'].is_digit():
-        s += ' FETCH NEXT {} ROWS ONLY'.format(d['fetch'])
-    return s
+    if 'offset' in d:
+        if isinstance(d['offset'], int) or d['offset'].is_digit():
+            query = query.offset(int(d['offset']))
+        else:
+            raise APIError('Invalid LIMIT: Expected a digit')
+    return query
 
 
 def parse_from_item(d):
@@ -217,69 +191,43 @@ def parse_from_item(d):
             [ LATERAL ] function_name ( [ argument [, ...] ] ) AS ( column_definition [, ...] )
     """
     # TODO: If 'type' is not set assume just a table name is present
+    if isinstance(d, str):
+        d = {'type': 'table', 'table': d}
     if d['type'] == 'table':
-        s = 'only ' if d.get('only', True) else ''
-        schema = read_pgid(d.pop('schema', ''))
-        if schema:
-            s += schema + "."
-        s += read_pgid(d['table'])
-        if 'star' in d and read_bool(d['star']):
-            s += ' * '
-        if 'alias' in d and read_bool(d['alias']):
-            s += ' AS ' + read_pgid(d['alias'])
-            s += ' (' + \
-                 ', '.join(
-                     map(read_pgid, d['alias'].pop('column_aliases', []))) + \
-                 ')'
-
+        schema_name = read_pgid(d.pop('schema', ''))
+        only = d.get('only', False)
+        table_name = read_pgid(d['table'])
+        table = Table(table_name, MetaData(bind=_get_engine()), schema=schema_name)
+        if 'alias' in d:
+            table = table.alias(read_pgid(d['alias']))
+        return table
     elif d['type'] == 'select':
-        s = 'LATERAL ' if read_bool(d['lateral']) else ''
-        s += '(' + parse_select(d['select']) + ') '
-        if 'alias' in d and read_bool(d['alias']):
-            s += ' AS ' + read_pgid(d['alias'])
-            s += ' (' + \
-                 ', '.join(
-                     map(read_pgid, d['alias'].pop('column_aliases', []))) + \
-                 ')'
-
+        return parse_select(d)
     elif d['type'] == 'join':
-        s = parse_from_item(d['left'])
-        if 'natural' in d and read_bool(d['natural']):
-            s += ' NATURAL'
-        if 'join_type' in d:
-            if d['join_type'].lower().strip() in ['join', 'inner join', 'left join',
-                                          'left outer join', 'right join',
-                                          'right outer join', 'full join',
-                                          'full inner join', 'full outer join', 'cross join']:
-                s += ' ' + d['join_type']
-            else:
-                raise APIError('Invalid join type')
-        else:
-            s += ' JOIN '
-
-        s += ' ' + parse_from_item(d['right'])
-
+        left = parse_from_item(d['left'])
+        right = parse_from_item(d['right'])
+        is_outer = parse_from_item(d['is_outer'])
+        full = parse_from_item(d['is_full'])
+        on_clause = None
         if 'on' in d:
-            s += ' ON ' + parse_condition(d['on'])
-        elif 'using' in d:
-            s += ' USING (' + ', '.join(map(read_pgid, d['using'])) + ')'
-    return s
+            on_clause = parse_condition(d['on'])
+        return left.join(right,onclause=on_clause, isouter=is_outer, full=full)
+    else:
+        raise APIError('Unknown from-item: ' +d['type'])
 
 
-def parse_expression(d, separator=', '):
+def parse_expression(d):
     # TODO: Implement
     if isinstance(d, dict):
         if d['type'] == 'column':
-            name = quote(d['column'])
+            name = d['column']
             if 'table' in d:
-                name = quote(d['table']) + '.' + name
+                name = d['table'] + '.' + name
                 if 'schema' in d:
-                    name = quote(d['schema']) + '.' + name
-            return name
+                    name = d['schema'] + '.' + name
+            return column(name)
         if d['type'] == 'grouping':
-            return '(' + parse_expression(d['grouping']) + ')'
-        if d['type'] == 'star':
-            return ' * '
+            return parse_expression(d['grouping'])
         if d['type'] == 'operator':
             return parse_operator(d)
         if d['type'] == 'operator_binary':
@@ -296,28 +244,28 @@ def parse_expression(d, separator=', '):
             else:
                 return 'null'
     if isinstance(d, list):
-        return separator.join(parse_expression(x) for x in d)
-    if isinstance(d, str):
-        return '\'' + d + '\''
-    else:
-        return d
+        return [parse_expression(x) for x in d]
+    return d
 
 
 def parse_condition(dl):
-    # TODO: Implement
     if type(dl) == dict:
         dl = [dl]
-
-    return " " + " AND ".join([parse_expression(d, separator=' AND ') for d in dl])
+    conditions = [parse_expression(d) for d in dl]
+    query = conditions[0]
+    for condition in conditions[1:]:
+        query=query._and(condition)
+    return query
 
 
 def parse_operator(d):
-    if d['operator'] == 'as':
-        return parse_expression(d['labeled']) + " AS " + d['label_name']
-    else:
-        return "%s %s %s" % (
-            parse_expression(d['left']), read_operator(d['operator'], d['right']),
-            parse_expression(d['right']))
+    query = parse_sqla_operator(d['operator'],
+                                parse_expression(d['left']),
+                                parse_expression(d['right']))
+    if 'as' in d:
+        query = query.label(d['as'])
+    return query
+
 
 
 def parse_operator_unary(d):
