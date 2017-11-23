305c02edc7875dbb9ffee5227ac19a860fe9a89f
MGlauer <martinglauer89@gmail.com>
2017-11-23 15:58:47 +0100

    Add schemas using alembic

---
+++ oedb_datamodels/versions/5c098aa81e2c_create_schemas.py
@@ -0,0 +1,42 @@
+"""create schemas
+
+Revision ID: 5c098aa81e2c
+Revises: 46fb02acc3b1
+Create Date: 2017-11-23 15:53:57.716306
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5c098aa81e2c'
+down_revision = '46fb02acc3b1'
+branch_labels = None
+depends_on = None
+
+schemas = [
+    "demand",
+    "economy",
+    "emission",
+    "environment",
+    "grid",
+    "boundaries",
+    "society",
+    "supply",
+    "scenario",
+    "climate",
+    "model_draft",
+    "openstreetmap",
+    "reference"
+]
+
+
+def upgrade():
+    for s in schemas:
+        op.execute("CREATE SCHEMA "+s)
+
+
+def downgrade():
+    for s in schemas:
+        op.execute("DROP SCHEMA "+s)
