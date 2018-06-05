0e78b4ef7ca7172d2cdf0834f45a14ca8869bb23
MGlauer <martinglauer89@gmail.com>
2018-06-05 19:57:32 +0200

    Use mutable dict for production

--- api/actions.py
+++ api/actions.py
@@ -70,11 +70,11 @@ def load_cursor(f):
         if fetch_all:
             context = open_raw_connection({}, {})
             context.update(open_cursor({}, context))
-            args[1].data['cursor_id'] = context['cursor_id']
             # django_restframework passes different data dictionaries depending
             # on the request type: PUT -> Mutable, POST -> Immutable
             # Thus, we have to replace the data dictionary by one we can mutate.
             args[1]._full_data = dict(args[1].data)
+            args[1].data['cursor_id'] = context['cursor_id']
         try:
             result = f(*args, **kwargs)
 
