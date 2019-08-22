a9e7ac7fbe566c1cd11c94a124f87f7094848b5d
MGlauer <martinglauer89@gmail.com>
2019-08-22 15:05:40 +0200

    Remove `sphinx.ext.pngmath` from docs
    This extension is not supported by readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
-    "sphinx.ext.pngmath",
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
