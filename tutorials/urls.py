df00a5319028e53826c1a4fd29ed39bb671b4911
jh-RLI <jonas.huber@rl-institut.de>
2019-12-04 12:32:21 +0100

    Add markdownx url, Add add-tutrorial url

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import include, url
+from markdownx import urls as markdownx
 
 from tutorials import views
 
 urlpatterns = [
     url(r'^$', views.ListTutorials.as_view()),
     url(r'^(?P<tutorial_id>[\w\-]+)/$', views.TutorialDetail.as_view()),
+    # this not working correctly - some error in gatherTutorials
+    url(r'/add/', views.NewTutorial.as_view(), name='add_tutorial'),
+    url(r'^markdownx/', include(markdownx)),
 ]
