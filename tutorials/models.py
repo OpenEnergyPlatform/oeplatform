327ba1797045235a420ce095d2cd2cac5257a1e9
jh-RLI <jonas.huber@rl-institut.de>
2019-12-04 12:03:57 +0100

    Add missing Fields according to mockup, Add markdownfield

--- tutorials/models.py
+++ tutorials/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from markdownx.models import MarkdownxField
 
 # Create your models here.
 
+
 class Tutorial(models.Model):
+    # ToDo: Fields that are out-commented are missing according to the mockup -> datamodel ??
+
+    # Category = models.TextField()
     title = models.TextField()
     html = models.TextField()
-    markdown = models.TextField()
+    markdown = MarkdownxField()
+    # Level = models.IntegerField()
