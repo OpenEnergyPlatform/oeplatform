7c09134bd4d126aba5c666c9c69f91198f0f8731
jh-RLI <jonas.huber@rl-institut.de>
2019-12-05 15:32:30 +0100

    Move markdownx url to oeplatform/urls.py

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import include, url
-from markdownx import urls as markdownx
 
 from tutorials import views
 
 urlpatterns = [
     url(r'^$', views.ListTutorials.as_view()),
     url(r'add/', views.NewTutorial.as_view(), name='add_tutorial'),
-    url(r'^markdownx/', include(markdownx)),
 
     # This must be last, otherwise it will match anything
-    url(r'^(?P<tutorial_id>[\w\-]+)/$', views.TutorialDetail.as_view()),
+    url(r'^(?P<tutorial_id>[\w\-]+)/$', views.TutorialDetail.as_view(), name='detail_tutorial'),
 ]
