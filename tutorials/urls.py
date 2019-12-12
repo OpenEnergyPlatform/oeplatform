7c1b539436b1f27896bc0e193b52838e2323519b
jh-RLI <jonas.huber@rl-institut.de>
2019-12-12 11:59:14 +0100

    Add url name to ListView, New url for delete view, Refactor ViewClass name for NewTutorials to CreateNewTutorials

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import include, url
 from tutorials import views
 
 urlpatterns = [
-    url(r'^$', views.ListTutorials.as_view()),
-    url(r'add/', views.NewTutorial.as_view(), name='add_tutorial'),
+    url(r'^$', views.ListTutorials.as_view(), name='list_tutorials'),
+    url(r'add/', views.CreateNewTutorial.as_view(), name='add_tutorial'),
     url(r'(?P<tutorial_id>[\w\-]+)/edit/', views.EditTutorials.as_view(), name='edit_tutorial'),
+    url(r'(?P<tutorial_id>[\w\-]+)/delete/', views.DeleteTutorial.as_view(), name='delete_tutorial'),
 
     # This must be last, otherwise it will match anything
     url(r'^(?P<tutorial_id>[\w\-]+)/$', views.TutorialDetail.as_view(), name='detail_tutorial'),
