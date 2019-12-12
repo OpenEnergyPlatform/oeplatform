c0c902cc356d1a3142a6d260e7b768114449013e
jh-RLI <jonas.huber@rl-institut.de>
2019-12-12 11:52:59 +0100

    Add options for choices fields, Add new fields to Tutorial model

--- tutorials/models.py
+++ tutorials/models.py
@@ -1,14 +1,22 @@
 from django.db import models
+from django.urls import reverse
+
 from markdownx.models import MarkdownxField
 
-# Create your models here.
+# add options if needed
+CATEGORY_OPTIONS = [('io', 'I/O'), ('intro', 'Introduction')]
+LEVEL_OPTIONS = [(1, '1'), (2, '2'), (3, '3')]
 
+# Create your models here.
 
 class Tutorial(models.Model):
     # ToDo: Fields that are out-commented are missing according to the mockup -> datamodel ??
 
-    # Category = models.TextField()
+    category = models.CharField(max_length=15, choices=CATEGORY_OPTIONS, blank=True)
     title = models.TextField()
     html = models.TextField()
     markdown = MarkdownxField()
-    # Level = models.IntegerField()
+    level = models.IntegerField(choices=LEVEL_OPTIONS, null=True)
+
+    def get_absolute_url (self):
+        return reverse('detail_tutorial', args=[self.id])
