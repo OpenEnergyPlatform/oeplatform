0447d5ee25697837546bf6a11404eefb86ac35cc
jh-RLI <jonas.huber@rl-institut.de>
2019-12-16 14:55:36 +0100

    Mention missing field in model

--- tutorials/models.py
+++ tutorials/models.py
@@ -9,12 +9,13 @@ LEVEL_OPTIONS = [(1, '1'), (2, '2'), (3, '3')]
 
 # Create your models here.
 
+
 class Tutorial(models.Model):
-    # ToDo: Fields that are out-commented are missing according to the mockup -> datamodel ??
 
     category = models.CharField(max_length=15, choices=CATEGORY_OPTIONS, blank=True)
     title = models.TextField()
     html = models.TextField()
+    # media = models.TextField()
     markdown = MarkdownxField()
     level = models.IntegerField(choices=LEVEL_OPTIONS, null=True)
 
