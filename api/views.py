8046c562bb0bf7aed050f7007601dc4fff9f4664
MGlauer <martinglauer89@gmail.com>
2020-04-23 21:57:20 +0200

    Handle errors in cursor wrapper

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ import json
 import logging
 import re
 import time
+import psycopg2
+
 from decimal import Decimal
 
 import geoalchemy2  # Although this import seems unused is has to be here
@@ -44,77 +46,92 @@ def transform_results(cursor, triggers, trigger_args):
     while row is not None:
         yield list(map(actions._translate_fetched_cell, row))
         row = cursor.fetchone()
-    for t in triggers:
-        t(*trigger_args)
+    for t, targs in zip(triggers, trigger_args):
+        t(*targs)
 
-def load_cursor(f):
-    def wrapper(*args, **kwargs):
-        artificial_connection = "connection_id" not in args[1].data
-        fetch_all = "cursor_id" not in args[1].data
-        triggered_close = False
-        if fetch_all:
 
-            # django_restframework passes different data dictionaries depending
-            # on the request type: PUT -> Mutable, POST -> Immutable
-            # Thus, we have to replace the data dictionary by one we can mutate.
-            if hasattr(args[1].data, "_mutable"):
-                args[1].data._mutable = True
-            context = {}
-            context["user"] = args[1].user
-            if not artificial_connection:
-                context["connection_id"] = args[1].data["connection_id"]
-            else:
-                context.update(actions.open_raw_connection({}, context))
-                args[1].data["connection_id"] = context["connection_id"]
-            if "cursor_id" in args[1].data:
-                context["cursor_id"] = args[1].data["cursor_id"]
-            else:
-                context.update(actions.open_cursor({}, context, named=True))
-                args[1].data["cursor_id"] = context["cursor_id"]
-        try:
-            result = f(*args, **kwargs)
+def load_cursor(named=False):
+    def inner(f):
+        def wrapper(*args, **kwargs):
+            artificial_connection = "connection_id" not in args[1].data
+            fetch_all = "cursor_id" not in args[1].data
+            triggered_close = False
             if fetch_all:
-                cursor = actions.load_cursor_from_context(context)
-                if not result:
-                    result = {}
-                # Initial server-side cursors do not contain any description before
-                # the first row is fetched. Therefore, we have to try to fetch the
-                # first one - if successful, we a description if not, nothing is returned.
-                # But: After the last row the cursor will 'forget' its description.
-                # Therefore we have to fetch the remaining data later.
 
-                # Set of triggers after all the data was fetched. The cursor must not be closed earlier!
-                triggers = [actions.close_cursor, actions.close_raw_connection]
-                trigger_args = [{}, context]
-                first = cursor.fetchone()
-                if first:
-                    first = map(actions._translate_fetched_cell, first)
-                    description = [
-                        [
-                            col.name,
-                            col.type_code,
-                            col.display_size,
-                            col.internal_size,
-                            col.precision,
-                            col.scale,
-                            col.null_ok,
-                        ]
-                        for col in cursor.description
-                    ]
-                    result["data"] = itertools.chain([first],transform_results(cursor, triggers, trigger_args))
-                    result["description"] = description
+                # django_restframework passes different data dictionaries depending
+                # on the request type: PUT -> Mutable, POST -> Immutable
+                # Thus, we have to replace the data dictionary by one we can mutate.
+                if hasattr(args[1].data, "_mutable"):
+                    args[1].data._mutable = True
+                context = {}
+                context["user"] = args[1].user
+                if not artificial_connection:
+                    context["connection_id"] = args[1].data["connection_id"]
+                else:
+                    context.update(actions.open_raw_connection({}, context))
+                    args[1].data["connection_id"] = context["connection_id"]
+                if "cursor_id" in args[1].data:
+                    context["cursor_id"] = args[1].data["cursor_id"]
+                else:
+                    context.update(actions.open_cursor({}, context, named=named))
+                    args[1].data["cursor_id"] = context["cursor_id"]
+            try:
+                result = f(*args, **kwargs)
+                if fetch_all:
+                    cursor = actions.load_cursor_from_context(context)
+                    session = actions.load_session_from_context(context)
+                    if not result:
+                        result = {}
+                    # Initial server-side cursors do not contain any description before
+                    # the first row is fetched. Therefore, we have to try to fetch the
+                    # first one - if successful, we a description if not, nothing is returned.
+                    # But: After the last row the cursor will 'forget' its description.
+                    # Therefore we have to fetch the remaining data later.
 
-                    result["rowcount"] = cursor.rowcount
-                    triggered_close=True
-        finally:
-            if not triggered_close:
-                if fetch_all and not artificial_connection:
-                    actions.close_cursor({}, context)
-                if artificial_connection:
-                    actions.close_raw_connection({}, context)
-        return result
+                    # Set of triggers after all the data was fetched. The cursor must not be closed earlier!
+                    triggers = [actions.close_cursor, actions.close_raw_connection, session.connection.commit]
+                    trigger_args = [({}, context), ({}, context), tuple()]
+                    first = None
+                    if not named or cursor.statusmessage:
+                        try:
+                            first = cursor.fetchone()
+                        except psycopg2.ProgrammingError as e:
+                            if not e.args or e.args[0] != "no results to fetch":
+                                raise e
+                        except psycopg2.errors.InvalidCursorName as e:
+                            print(e)
+                    if first:
+                        first = map(actions._translate_fetched_cell, first)
+                        if cursor.description:
+                            description = [
+                                [
+                                    col.name,
+                                    col.type_code,
+                                    col.display_size,
+                                    col.internal_size,
+                                    col.precision,
+                                    col.scale,
+                                    col.null_ok,
+                                ]
+                                for col in cursor.description
+                            ]
+                            result["data"] = (x for x in itertools.chain([first],transform_results(cursor, triggers, trigger_args)))
+                            result["description"] = description
 
-    return wrapper
+                            result["rowcount"] = cursor.rowcount
+                            triggered_close = True
+                    if not triggered_close and artificial_connection:
+                        session.connection.commit()
+            finally:
+                if not triggered_close:
+                    if fetch_all and not artificial_connection:
+                        actions.close_cursor({}, context)
+                    if artificial_connection:
+                        actions.close_raw_connection({}, context)
+            return result
+
+        return wrapper
+    return inner
 
 
 def cors(allow):
@@ -194,13 +211,13 @@ class Sequence(APIView):
             raise PermissionDenied
         return self.__delete_sequence(request, schema, sequence, request.data)
 
-    @load_cursor
+    @load_cursor()
     def __delete_sequence(self, request, schema, sequence, jsn):
         seq = sqla.schema.Sequence(sequence, schema=schema)
         seq.drop(bind=actions._get_engine())
         return JsonResponse({}, status=status.HTTP_200_OK)
 
-    @load_cursor
+    @load_cursor()
     def __create_sequence(self, request, schema, sequence, jsn):
         seq = sqla.schema.Sequence(sequence, schema=schema)
         seq.create(bind=actions._get_engine())
@@ -217,7 +234,7 @@ class Metadata(APIView):
 
     @api_exception
     @require_write_permission
-    @load_cursor
+    @load_cursor()
     def post(self, request, schema, table):
         table_obj = actions._get_table(schema=schema, table=table)
         raw_input = request.data
@@ -379,7 +396,7 @@ class Table(APIView):
             if not colname.isidentifier():
                 raise APIError("Invalid column name: %s" % colname)
 
-    @load_cursor
+    @load_cursor()
     def __create_table(
         self, request, schema, table, column_definitions, constraint_definitions, metadata=None
     ):
@@ -577,8 +594,12 @@ class Rows(APIView):
 
         return_obj = self.__get_rows(request, data)
         # Extract column names from description
-        cols = [col[0] for col in return_obj["description"]]
-
+        if "description" in return_obj:
+            cols = [col[0] for col in return_obj["description"]]
+        else:
+            cols = []
+            return_obj["data"] = []
+            return_obj["rowcount"] = 0
         if format == "csv":
             pseudo_buffer = Echo()
             writer = csv.writer(pseudo_buffer, quoting=csv.QUOTE_ALL)
@@ -678,7 +699,7 @@ class Rows(APIView):
         actions.apply_changes(schema, table)
         return JsonResponse(result)
 
-    @load_cursor
+    @load_cursor()
     def __delete_rows(self, request, schema, table, row_id=None):
         where = request.GET.getlist("where")
         query = {"schema": schema, "table": table}
@@ -729,7 +750,7 @@ class Rows(APIView):
                     )
         return where_clauses
 
-    @load_cursor
+    @load_cursor()
     def __insert_row(self, request, schema, table, row, row_id=None):
         if row_id and row.get("id", int(row_id)) != int(row_id):
             return actions._response_error(
@@ -756,7 +777,7 @@ class Rows(APIView):
 
         return result
 
-    @load_cursor
+    @load_cursor()
     def __update_rows(self, request, schema, table, row, row_id=None):
         context = {
             "connection_id": request.data["connection_id"],
@@ -787,7 +808,7 @@ class Rows(APIView):
 
         return actions.data_update(query, context)
 
-    @load_cursor
+    @load_cursor(named=True)
     def __get_rows(self, request, data):
         table = actions._get_table(data["schema"], table=data["table"])
         params = {}
@@ -871,7 +892,7 @@ def create_ajax_handler(func, allow_cors=False, requires_cursor=False):
 
         def execute(self, request):
             if requires_cursor:
-                return load_cursor(self._internal_execute)(self, request)
+                return load_cursor()(self._internal_execute)(self, request)
             else:
                 return self._internal_execute(request, request)
 
