94b79be2e92ed5134787f9e6ec859858d4ea0769
Johann Wagner <johann@wagnerdevelopment.de>
2020-06-11 12:00:22 +0200

    Remove martor due to styling bugs and added martor

--- tutorials/models.py
+++ tutorials/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-from martor.models import MartorField
-
-
 # add options if needed
 CATEGORY_OPTIONS = [('io', 'I/O'), ('intro', 'Introduction')]
 LEVEL_OPTIONS = [(1, 'Beginners'), (2, 'Intermediates'), (3, 'Advanced')]
@@ -14,10 +11,10 @@ LEVEL_OPTIONS = [(1, 'Beginners'), (2, 'Intermediates'), (3, 'Advanced')]
 class Tutorial(models.Model):
 
     category = models.CharField(max_length=15, choices=CATEGORY_OPTIONS, blank=True)
-    title = models.TextField()
+    title = models.CharField(max_length=255)
     html = models.TextField()
     # media = models.TextField()
-    markdown = MartorField()
+    markdown = models.TextField()
     level = models.IntegerField(choices=LEVEL_OPTIONS, null=True)
 
     def get_absolute_url (self):
