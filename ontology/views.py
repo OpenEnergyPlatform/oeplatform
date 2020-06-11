2f12af5f52a41a21c4b65128571f14744277bdf2
MGlauer <martinglauer89@gmail.com>
2020-06-11 11:25:20 +0200

    Revert direct file use

--- ontology/views.py
+++ ontology/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.utils.encoding import smart_str
 from django.views import View
 from rdflib import Graph, RDFS
 from oeplatform.settings import ONTOLOGY_FOLDER
@@ -92,13 +91,13 @@ class OntologyStatics(View):
         else:
             file_path = f"{ONTOLOGY_FOLDER}/{ontology}/{version}/{file}.{extension}"
         if os.path.exists(file_path):
-            response = HttpResponse(content_type="application/rdf+xml")
-            response["Content-Disposition"] = f'attachment; filename="{file}.{extension}"'
-            response['X-Sendfile'] = smart_str(file_path)
-            return response
+            with open(file_path) as f:
+                response = HttpResponse(f, content_type="application/rdf+xml; charset=utf-8")
+                response["Content-Disposition"] = f'attachment; filename="{file}.{extension}"'
+                return response
         else:
             file_path = f"{ONTOLOGY_FOLDER}/{ontology}/{version}/modules/{file}.{extension}"
-            response = HttpResponse(content_type="application/rdf+xml")
-            response["Content-Disposition"] = f'attachment; filename="{file}.{extension}"'
-            response['X-Sendfile'] = smart_str(file_path)
-            return response
+            with open(file_path) as f:
+                response = HttpResponse(f, content_type="application/rdf+xml; charset=utf-8")
+                response["Content-Disposition"] = f'attachment; filename="{file}.{extension}"'
+                return response
