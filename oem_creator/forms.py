d45d74bc1b5212820f4e2574ad6036dcaaa38536
Tu Phan Ngoc <RL-INSTITUT\tuphan.ngoc@rli-nb-65.rl-institut.local>
2020-09-21 17:11:46 +0200

    add png data to explain the metadata tool , add some feature to summarize it in a easier way (putting away the editjson button) download button and differnts colors for backgroun are still missing

--- oem_creator/forms.py
+++ oem_creator/forms.py
@@ -7,7 +7,9 @@ from django_jsonforms.forms import JSONSchemaField
 options = {
         'theme': 'bootstrap4',
         'disable_collapse': True,
-        'disable_properties': True
+        'disable_properties': True,
+        'compact': True,
+        'disable_array_reorder': True
 }
 class CreatorForm(Form):
     Meta_v_1_4_0 = JSONSchemaField(schema='oem_v_1_4_0.json', options=options)
