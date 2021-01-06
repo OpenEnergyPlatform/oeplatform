3dae8df1b6f0cb9397cdb84efd01e0499ab7aaa9
MGlauer <martinglauer89@gmail.com>
2021-01-06 10:31:43 +0100

    Include study in tag exemption

--- modelview/views.py
+++ modelview/views.py
@@ -236,7 +236,7 @@ def editModel(request, model_name, sheettype):
     model = get_object_or_404(c, pk=model_name)
 
     tags = []
-    if sheettype == "scenario":
+    if sheettype in ["scenario", "study"]:
         pass
     else:
         d = load_tags()
