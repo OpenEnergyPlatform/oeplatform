5dc4c7f0c5aef85ada82626511a543dd1b6d6f11
jh-RLI <jonas.huber@rl-institut.de>
2021-03-17 12:22:44 +0100

    update form field name

--- tutorials/forms.py
+++ tutorials/forms.py
@@ -7,4 +7,4 @@ class TutorialForm(forms.ModelForm):
     class Meta:
         model = Tutorial
         fields = ('category', 'title', 'markdown', 'media_src', 'level',
-                  'language', 'medium', 'license', 'creator', 'email_contact', 'github')
+                  'language', 'medium', 'license', 'creator', 'email_contact', 'github_contact')
