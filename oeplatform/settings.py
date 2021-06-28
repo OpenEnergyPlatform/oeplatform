d2d611f2b119744f6b1a2564addd97e953bedfe1
Johann Wagner <johann@wagnerdevelopment.de>
2021-06-28 17:46:13 +0200

    Reenabled axes

--- oeplatform/settings.py
+++ oeplatform/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
 }
 AUTHENTICATION_BACKENDS = [
     # AxesBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
-    # 'axes.backends.AxesBackend',
+    'axes.backends.AxesBackend',
 
     # Django ModelBackend is the default authentication backend.
     'django.contrib.auth.backends.ModelBackend',
