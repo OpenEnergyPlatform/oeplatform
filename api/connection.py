d023ed290a2b6b6ee7b5d6226143e339cbbfa0d4
Christian Winger <c@wingechr.de>
2022-06-11 11:01:36 +0100

    formatting

--- api/connection.py
+++ api/connection.py
@@ -1,13 +1,17 @@
 """Contains functions to interact with the postgres oedb"""
 
 import sqlalchemy as sqla
+
 from api import DEFAULT_SCHEMA
+from dataedit.models import Schema, Table
 
 try:
     import oeplatform.securitysettings as sec
-except:
+except Exception:
     import logging
+
     logging.error("No securitysettings found. Triggerd in api/connection.py")
+
 
 def get_connection_string():
     return "postgresql://{0}:{1}@{2}:{3}/{4}".format(
@@ -34,11 +38,30 @@ def table_exists_in_oedb(table, schema=None):
     Returns:
         bool
     """
-    engine = _get_engine()
     schema = schema or DEFAULT_SCHEMA
+    engine = _get_engine()
     conn = engine.connect()
     try:
         result = engine.dialect.has_table(conn, table, schema=schema)
     finally:
         conn.close()
     return result
+
+
+def table_exists_in_django(table, schema=None):
+    """check if table exists in django
+
+    Args:
+        table (str): table name
+        schema (str, optional): table schema name
+
+    Returns:
+        bool
+    """
+    schema = schema or DEFAULT_SCHEMA
+    schema_obj = Schema.objects.get_or_create(name=schema)[0]
+    try:
+        Table.objects.get(name=table, schema=schema_obj)
+        return True
+    except Table.DoesNotExist:
+        return False
--- api/tests/test_regression/test_issue_807_table_artefacts.py
+++ api/tests/test_regression/test_issue_807_table_artefacts.py
@@ -1,36 +1,29 @@
-"""Some api actions run `assert_permission`, which calls 
+"""Some api actions run `assert_permission`, which calls
 DBTable.load(schema, table), which used get_or_create().
 
 This caused the creation of an artefact entry in django tables.
 
 """
-from api.tests import APITestCase
-from api.connection import table_exists_in_oedb
 from api.actions import assert_permission
-from dataedit.models import Table, Schema
+from api.connection import table_exists_in_django, table_exists_in_oedb
+from api.tests import APITestCase
 
-def table_exists_in_django(table, schema):
-    schema_obj = Schema.objects.get_or_create(name=schema)[0]
-    table_objs = Table.objects.get(name=table, schema=schema_obj)
-    return bool(table_objs)
-    
 
 class Test_issue_807_table_artefacts(APITestCase):
-    schema = "test" # created in APITestCase
+    schema = "test"  # created in APITestCase
     table = "nonexisting_table"
 
     def test_issue_807_table_artefacts(self):
         try:
             assert_permission(
-                user=self.user, # created in APITestCase
+                user=self.user,  # created in APITestCase
                 schema=self.schema,
                 table=self.table,
-                permission=0 # any value will do
+                permission=0,  # any value will do
             )
         except Exception:
             pass
-        
+
         self.assertFalse(table_exists_in_oedb(self.table, self.schema))
         # this failed before the bugfix
         self.assertFalse(table_exists_in_django(self.table, self.schema))
-        
