9771c35c6c2d13e1ae521257a3eec23c29aede64
Christian Winger <c@wingechr.de>
2022-08-12 15:35:44 +0100

    auto create and check for numerical id column as primary key

--- api/actions.py
+++ api/actions.py
@@ -749,12 +749,12 @@ def assert_valid_table_name(table):
                        "and start with a letter.")
 
 
-def table_create(schema, table, columns, constraints_definitions, cursor, table_metadata=None):
+def table_create(schema, table, column_definitions, constraints_definitions, cursor, table_metadata=None):
     """
     Creates a new table.
     :param schema: schema
     :param table: table
-    :param columns: Description of columns
+    :param column_definitions: Description of columns
     :param constraints: Description of constraints
     :return: Dictionary with results
     """
@@ -796,7 +796,22 @@ def table_create(schema, table, columns, constraints_definitions, cursor, table_
 
     metadata = MetaData()
 
-    columns = [get_column_definition_query(c) for c in columns]
+    primary_key_col_names = None
+    columns_by_name = {}    
+    
+    columns = []
+    for cdef in column_definitions:
+        col = get_column_definition_query(cdef)
+        columns.append(col)
+
+        # check for duplicate column names
+        if col.name in columns_by_name:
+            raise APIError("Duplicate column name: %s" % col.name)
+        columns_by_name[col.name] = col
+        if col.primary_key:
+            if primary_key_col_names:
+                raise APIError("Multiple definitions of primary key")
+            primary_key_col_names = [col.name]
 
     constraints = []
 
@@ -814,8 +829,14 @@ def table_create(schema, table, columns, constraints_definitions, cursor, table_
             if "columns" in constraint:
                 ccolumns = constraint["columns"]
             else:
-                ccolumns = [constraint["constraint_parameter"]]
-            constraints.append(sa.schema.PrimaryKeyConstraint(*ccolumns, **kwargs))
+                ccolumns = [constraint["constraint_parameter"]]                        
+            
+            if primary_key_col_names:
+                raise APIError("Multiple definitions of primary key")
+            primary_key_col_names = ccolumns
+            
+            const = sa.schema.PrimaryKeyConstraint(*ccolumns, **kwargs)            
+            constraints.append(const)
         elif constraint_type == "unique":
             kwargs = {}
             cname = constraint.get("name")
@@ -828,6 +849,28 @@ def table_create(schema, table, columns, constraints_definitions, cursor, table_
             constraints.append(sa.schema.UniqueConstraint(*ccolumns, **kwargs))
 
     assert_valid_table_name(table)
+
+    # autogenerate id column if missing
+    if 'id' not in columns_by_name:        
+        columns_by_name["id"] = sa.Column("id", sa.BigInteger, autoincrement=True)
+        columns.insert(0, columns_by_name["id"])
+    
+    # check id column type
+    id_col_type = str(columns_by_name["id"].type).upper()
+    if not "INT" in id_col_type or "SERIAL" in id_col_type:
+        raise APIError("Id column must be of int type")
+    
+    # autogenerate primary key
+    if not primary_key_col_names:
+        constraints.append(sa.schema.PrimaryKeyConstraint("id"))
+        primary_key_col_names = ["id"]
+    
+    # check pk == id
+    if tuple(primary_key_col_names) != ("id",):
+        raise APIError("Primary key must be column id")
+
+    
+
     t = Table(table, metadata, *(columns + constraints), schema=schema, comment=comment_on_table)
     t.create(_get_engine())
 
