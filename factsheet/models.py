f9b4a494135fb313ba710c53ab1a241ec6825fb7
Adel Memariani <memarian@haskell2go.iks.cs.ovgu.de>
2022-09-15 17:33:08 +0200

    Facsheets based on JSON-LD

--- factsheet/models.py
+++ factsheet/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
-class UserModel(models.Model):
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
+class FundingSourceModel(models.Model):
+    name = models.CharField(max_length=100)
+    uri = models.URLField(max_length=400)
+
+class FactsheetModel(models.Model):
+    name = models.CharField(max_length=100)
+    uri = models.URLField(max_length=400)
+    funding_source = models.ForeignKey(FundingSourceModel, on_delete = models.CASCADE)
--- factsheet/schema.py
+++ factsheet/schema.py
@@ -1,47 +1,77 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from factsheet.models import UserModel
+from factsheet.models import FactsheetModel
+from factsheet.models import FundingSourceModel
 
-class UserType(DjangoObjectType):
+class FundingSourceType(DjangoObjectType):
     class Meta:
-        model = UserModel
+        model = FundingSourceModel
 
+class FundingSourceInput(graphene.InputObjectType):
+    id = graphene.Int(required=True)
+    name = graphene.String(required=True)
+    uri =  graphene.String(required=True)
+
+class FactsheetType(DjangoObjectType):
+    class Meta:
+        model = FactsheetModel
+
+class FactsheetInput(graphene.InputObjectType):
+    name = graphene.String(required=True)
+    uri =  graphene.String(required=True)
+    funding_sources = graphene.List(graphene.Int)
 
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
-    user_by_id = graphene.Field(UserType, id=graphene.Int())
+    factsheets = graphene.List(FactsheetType)
+    factsheet_by_id = graphene.Field(FactsheetType, id=graphene.Int())
+    funding_sources = graphene.List(FundingSourceType)
 
-    def resolve_users(self, info):
-        return UserModel.objects.all()
+    def resolve_factsheets(self, info):
+        return FactsheetModel.objects.all()
 
-    def resolve_user_by_id(self, info, id):
-        return UserModel.objects.get(id=id)
+    def resolve_factsheet_by_id(self, info, id):
+        return FactsheetModel.objects.get(id=id)
 
+    def resolve_funding_sources(self, info):
+        return FundingSourceModel.objects.all()
 
-class CreateUser(graphene.Mutation):
-    id = graphene.Int()
-    firstName = graphene.String()
-    lastName = graphene.String()
+class CreateFundingSource(graphene.Mutation):
+    name = graphene.String()
+    uri = graphene.String()
 
     class Arguments:
-        firstName = graphene.String()
-        lastName = graphene.String()
+        name = graphene.String()
+        uri = graphene.String()
 
-    def mutate(self, info, firstName, lastName):
-        user = UserModel(firstName=firstName, lastName=lastName)
-        user.save()
+    def mutate(self, info, name, uri):
+        funding_source = FundingSourceModel(name=name, uri=uri)
+        funding_source.save()
 
-        return CreateUser(
-            id=user.id,
-            firstName=user.firstName,
-            lastName=user.lastName,
+        return CreateFundingSource(
+            name=funding_source.name,
+            uri=funding_source.uri,
         )
+
+class CreateFactsheet(graphene.Mutation):
+    name = graphene.String()
+    uri = graphene.String()
+    funding_sources = graphene.List(FundingSourceType)
+
+    class Arguments:
+        name = graphene.String()
+        uri = graphene.String()
+        funding_sources = graphene.List(FundingSourceInput)
+
+    def mutate(self, info, name, uri, funding_sources):
+        qs = FactsheetModel.objects.filter(funding_source=5)
+        funding_source_list = [FactsheetModel.objects.filter(funding_source=funding_source.id).update_or_create(**funding_source) for funding_source in funding_sources]
+        return CreateFactsheet(funding_sources=funding_source_list)
 
 
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-
+    create_factsheet = CreateFactsheet.Field()
+    create_funding_source = CreateFundingSource.Field()
 
 schema = graphene.Schema(
     query=Query,
