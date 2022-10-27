d7aa73796ac72c9346c36a1c8eaec69dbc4c6fee
Adel Memariani <memarian@winadms-MacBook-Pro.local>
2022-10-27 19:14:16 +0200

    add new template for react

--- factsheet/views.py
+++ factsheet/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def factsheets_index(request, *args, **kwargs):
-    context_dict = {
-        "user": 'Adel',
-    }
-    return render(request, 'factsheet/index.html', context='context')
+    return render(request, 'factsheet/index.html')
