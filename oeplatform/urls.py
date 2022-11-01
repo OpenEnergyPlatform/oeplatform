8b03211c72ffa387583460ae75b1d2b114d51bcd
jh-RLI <jonas.huber@rl-institut.de>
2022-11-01 14:51:50 +0100

    code formatting

--- oeplatform/urls.py
+++ oeplatform/urls.py
@@ -32,5 +32,8 @@ urlpatterns = [
     url(r"^ontology/", include("ontology.urls")),
     url(r"^tutorials/", include("tutorials.urls")),
     url(r"^viewer/oeo/", include("oeo_viewer.urls")),
-    url(r"^tutorials/[/\w_\d]*/images/(?P<f>[\w_\d]+(\.[\w_\d]+)?)$", ImagesView.as_view()),
+    url(
+        r"^tutorials/[/\w_\d]*/images/(?P<f>[\w_\d]+(\.[\w_\d]+)?)$",
+        ImagesView.as_view(),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
