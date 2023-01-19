e537e199829ded477f9dad1b6fcdadf8e29d0fb0
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-01-19 13:42:05 +0100

    Refactored JS from html into peer_review.js

--- dataedit/static/peer_review/peer_review.js
+++ dataedit/static/peer_review/peer_review.js
@@ -1,3 +1,4 @@
+
 var peerReview = function (config) {
     var state = {
         "topic": null,
@@ -100,3 +101,237 @@ var peerReview = function (config) {
     return config;
 
 }
+
+        // 1 Identify Category from tab
+        var selectedCategory;
+        document.getElementById('general').addEventListener('click', category_identifier);
+        var category_identifier = function (inhtml) {
+
+            // const selectedCategory = document.querySelector("#general-tab").innerHTML;
+            const x = inhtml;
+            console.log(x)
+            const y = document.querySelector(inhtml).innerHTML;
+            selectedCategory = x;
+
+
+
+            return selectedCategory;
+        }
+
+
+        // 1 Identify field name
+        var selectedField;
+        var selectedFieldValue;
+        //document.getElementById('field_name').addEventListener('click', click_field);
+        var click_field = function (fieldKey, fieldValue) {
+            const x = fieldKey;
+            selectedField = x;
+            selectedFieldValue=fieldValue;
+            const selectedName = document.querySelector("#review-field-name");
+            selectedName.textContent = fieldKey + ' ' + fieldValue;
+
+            return x
+        }
+
+
+        // 2 Identify selected button
+        var selectedState;
+document.getElementById('ok-button').addEventListener('click', function(){
+            selectedState = "ok";
+            selectedButton();
+        });
+document.getElementById('suggestion-button').addEventListener('click', function(){
+            selectedState = "suggestion";
+            selectedButton();
+        });
+document.getElementById('rejected-button').addEventListener('click', function(){
+            selectedState = "rejected";
+            selectedButton();
+        });
+
+
+function selectedButton() {
+    console.log(selectedState);
+}
+
+        // 3 Save added comment for corresponding field
+        // document.getElementById('commentarea').addEventListener('change', saveComment);
+        var saveComment = function() {
+            var z = document.getElementById('commentarea');
+            return z.value
+        }
+
+
+        // 3 Save added value suggestion for corresponding field
+        // document.getElementById('valuearea').addEventListener('change', saveValue);
+        var saveValue = function() {
+            var selectedValue = document.querySelector("#valuearea");
+
+            return selectedValue.value
+        }
+
+
+        // Function to save all values
+        document.getElementById('submit-button').addEventListener('click', saveEntrances);
+          var saveEntrances = function(button) {
+            const state = button;
+            //selectedState = state;
+	        // Fill local Storage variable with content of user input upon Button click
+            // Create list for review fields if it doesn't exist yet
+            if(Object.keys(current_review["reviews"]).length === 0
+                && current_review["reviews"].constructor === Object){
+                    current_review["reviews"] = [];
+            }
+            if(selectedField){
+                //TODO check if object with selectedField already exists (using for loop)
+                //TODO turn into function updateReviewVariable()
+                var element = document.querySelector('[aria-selected="true"]');
+                var category = (element.getAttribute("data-bs-target"));
+                current_review["reviews"].push(
+                    {
+                        "category": category,
+                        "key": selectedField,
+                        "fieldReview": {
+                            "timestamp": null, //TODO put actual timestamp
+                            "user": "oep_reviewer", //TODO put actual username
+                            "role": "reviewer",
+                            "contributorValue": selectedFieldValue,
+                            "comment": document.getElementById("commentarea").value,
+                            "reviewerSuggestion": document.getElementById("valuearea").value,
+                            "state": selectedState,
+                        }
+                    }
+                );
+            }
+            //alert(JSON.stringify(current_review, null, 4));
+            document.getElementById("summary").innerHTML = (JSON.stringify(current_review, null, 4))
+
+          }
+
+/*        var saveEntrances = function() {
+            var category = category_identifier();
+            var fieldName = selectedField;
+            var state = selectedState;
+            let date = new Date();
+            var comment = saveComment();
+            var value_suggestion = saveValue();
+
+
+            if (selectedState == "ok") {
+                var review_result = {
+                    "category": category,
+                    "key": fieldName,
+                    "state": state,
+                    "entry":null,
+                    "loop": null
+                }
+            }
+            else {var review_result = {
+                    "category": category,
+                    "key": fieldName,
+                    "state": state,
+                    "fieldReview": [
+                        {
+                            "timestamp": date,
+                            "reviewer": "oep_user",
+                            "comment": comment,
+                            "value_suggestion": value_suggestion,
+                            "accepted": null
+                        }
+                    ]
+                }
+            }
+            document.getElementById("commentarea").value = "";
+            document.getElementById("valuearea").value = "";
+
+
+            console.log(review_result)
+            return review_result
+        }
+*/
+
+
+
+
+
+
+
+
+
+
+        // ----------------------------------------------------------------------
+
+
+        // Function for OK-Button
+        document.getElementById('ok-button').addEventListener('click', okFunction)
+        var okFunction = function(fieldKey, fieldValue) {
+            var category = category_identifier();
+
+            var review_result = {
+                "category": category,
+                "key": fieldKey,
+                "state": "ok",
+                "loop": null
+            }
+
+            return review_result
+        }
+
+
+        // Function for SUGGESTION-Button
+        document.getElementById('suggestion-button').addEventListener('click', suggestionFunction)
+        var suggestionFunction = function(fieldKey, fieldValue) {
+            let date = new Date();
+            var category = category_identifier();
+            var comment = saveComment();
+            var value_suggestion = saveValue();
+
+
+            var review_result = {
+                "category": category,
+                "key": fieldKey,
+                "state": "suggestion",
+                "loop": [
+                    {
+                        "timestamp": date,
+                        "reviewer": "oep_user",
+                        "comment": comment,
+                        "value_suggestion": value_suggestion,
+                        "accepted": null
+                    }
+                ]
+            }
+
+            return review_result
+        }
+
+
+        // Function for REJECTED-Button
+        document.getElementById('rejected-button').addEventListener('click', rejectedFunction)
+        var rejectedFunction = function(fieldKey, fieldValue) {
+            var fieldKey = fieldKey;
+            let date = new Date();
+            var category = category_identifier();
+            var comment = saveComment();
+            var value_suggestion = saveValue();
+
+            var review_result = {
+                "category": category,
+                "key": fieldKey,
+                "state": "rejected",
+                "loop": [
+                    {
+                        "timestamp": date,
+                        "reviewer": "oep_user",
+                        "comment": comment,
+                        "value_suggestion": value_suggestion,
+                        "accepted": null
+                    }
+                ]
+            }
+
+            return review_result
+        }
+
+
+peerReview(config);
