6b1112432cf62c92cc70edb693215be56d07f187
Adel Memariani <memarian@haskell2go.iks.cs.ovgu.de>
2023-05-03 17:09:42 +0200

    changed model.py

--- factsheet/models.py
+++ factsheet/models.py
@@ -1,6 +1,4 @@
 from django.db import models
 
 class Factsheet(models.Model):
-    id = models.UUIDField(primary_key=True)
-    acronym = models.CharField(default="Factsheet's acronym", max_length=100)
     factsheetData = models.JSONField(default=dict)
