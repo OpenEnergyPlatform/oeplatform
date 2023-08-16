06f3a8d054e28b1b67f40eb6bd03afc1e0be5f8c
Christian Winger <c@wingechr.de>
2023-08-16 13:06:38 +0100

    ensure schemas in oedb

---
+++ oedb_datamodels/versions/4f01a68dcf6f_prepare_schema_dissolve.py
@@ -0,0 +1,29 @@
+"""empty message
+
+Revision ID: 4f01a68dcf6f
+Revises: 3c2369dfcc55
+Create Date: 2023-08-16 13:57:22.942292
+
+"""
+# import sqlalchemy as sa
+from alembic import op
+
+from oeplatform.settings import MANAGED_SCHEMAS
+
+# revision identifiers, used by Alembic.
+revision = "4f01a68dcf6f"
+down_revision = "3c2369dfcc55"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    for s in MANAGED_SCHEMAS:
+        op.execute("CREATE SCHEMA IF NOT EXISTS " + s)
+    for s in MANAGED_SCHEMAS:
+        op.execute("CREATE SCHEMA IF NOT EXISTS " + s)
+
+
+def downgrade():
+    # do not delete schemas
+    pass
