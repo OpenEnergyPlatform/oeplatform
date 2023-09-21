5a8dbcb4ccb16cf59325c3bf6b623a4932314d70
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-09-21 16:52:05 +0100

    create theme components folder

---
+++ factsheet/frontend/src/styles/oep-theme/components/buttonStyles.js
@@ -0,0 +1,13 @@
+const buttonStyles = {
+  defaultProps: {
+    disableElevation : true
+  },
+  styleOverrides: {
+    root: {
+      textTransform: 'capitalize',
+      minWidth: 'fit-content',
+    }
+  }
+}
+
+export default buttonStyles;
