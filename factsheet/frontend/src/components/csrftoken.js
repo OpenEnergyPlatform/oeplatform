5bb0efba04e753d806dfc7b5d5b72d66d8af487b
Jonas Huber <jonas.huber@rl-institut.de>
2023-10-26 11:30:05 +0200

    add react component that gets djangos csrf cookie value to add it ti axios post requests headers #1299

---
+++ factsheet/frontend/src/components/csrftoken.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+/**
+ * Returns name from cookies
+ * @param {string} name Key to look up in cookie
+ * @returns {value} Cookie value
+ */
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === name + '=') {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  
+/**
+ * Get CSRF Token
+ * @returns {string} CSRF Token
+ */
+function getCsrfToken() {
+var token1 = getCookie("csrftoken");
+return token1;
+}
+console.log(getCsrfToken());
+const CSRFToken = () => {
+    const csrftoken = getCsrfToken(); // Get the CSRF token using the getCsrfToken function
+    return csrftoken; // Return the token as a string
+};
+
+export default CSRFToken;
