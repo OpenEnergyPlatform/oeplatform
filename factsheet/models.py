c03299565dc161007900b5fca656b399d1563fa7
Jonas Huber <jonas.huber@rl-institut.de>
2023-10-26 11:27:10 +0200

    add model to simplify the access controle to scenario bundles by user id and bundle uid #1299

--- factsheet/models.py
+++ factsheet/models.py
@@ -9,3 +9,13 @@ class HistoryOfOEKG(models.Model):
     type_of_action = models.CharField(max_length=100)
     user = models.CharField(max_length=200)
     timestamp = models.DateTimeField(default=timezone.now)
+
+
+class ScenarioBundleAccessControl(models.Model):
+    owner_user = models.ForeignKey(
+        "login.myuser",
+        on_delete=models.CASCADE,
+        related_name="scenario_bundle_creator",
+        null=False,
+    )
+    uid = models.TextField()
