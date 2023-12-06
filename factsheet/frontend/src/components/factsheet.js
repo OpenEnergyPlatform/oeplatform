6b4280e307d22d029130c7f867f634b24efd7464
Adel Memariani <adel.memariani@ovgu.de>
2023-12-06 13:19:56 +0100

    add clickable study descriptors #1478

--- factsheet/frontend/src/components/factsheet.js
+++ factsheet/frontend/src/components/factsheet.js
@@ -231,21 +231,21 @@ function Factsheet(props) {
 
 
   const StudyKeywords = [
-    'resilience',
-    'life cycle analysis',
-    'CO2 emissions',
-    'Greenhouse gas emissions',
-    'Reallabor',
-    '100% renewables',
-    'acceptance',
-    'sufficiency',
-    '(changes in) demand',
-    'degree of electrifiaction',
-    'regionalisation',
-    'total gross electricity generation',
-    'total net electricity generation',
-    'peak electricity generation',
-    'study report due to leggal obligation'
+    ['resilience', 'http://openenergy-platform.org/ontology/oeo/OEO_00360015', 'Resilience is a disposition of a system that represents the capacity of a system to absorb disturbance and reorganize so as to retain essentially the same function, structure, and feedbacks.'],
+    ['life cycle analysis', 'http://www.openenergy-platform.org/ontology/oeo/OEO_00330023', 'A life cycle assessment is a methodology to calculate and analyse environmental impacts of the life cycle of a material entity or process.'],
+    ['CO2 emissions', '', ''],
+    ['Greenhouse gas emissions', '', ''],
+    ['Reallabor', '', ''],
+    ['100% renewables', '', ''],
+    ['acceptance', 'http://openenergy-platform.org/ontology/oeo/OEO_00360000', 'Acceptance is a realizable entity that represents the attitude of a person or organisation with respect to a certain constructional, (infra)structural or political measure that may be realized in, affected by or results of complex processes like discussions, communications, transformative measures or former personal experiences.'],
+    ['sufficiency', 'http://openenergy-platform.org/ontology/oeo/OEO_00010444', 'Sufficiency is a plan specification for reducing, in absolute terms, the consumption and production of end-use products and services through changes in social practices in order to comply with environmental sustainability while ensuring an adequate social foundation for all people.'],
+    ['(changes in) demand', '', ''],
+    ['degree of electrifiaction', '', ''],
+    ['regionalisation', 'http://openenergy-platform.org/ontology/oeo/OEO_00340006', 'Regionalisation is a methodology to calculate spatially distributed energy producers and consumers with the aim to highlight regional differences in energy supply and potentials, particularly related to renewable energies.'],
+    ['total gross electricity generation', '', ''],
+    ['total net electricity generation', '', ''],
+    ['peak electricity generation', '', ''],
+    ['study report due to leggal obligation', 'http://openenergy-platform.org/ontology/oeo/OEO_00020373', 'A study report due to legal obligation is a study report that is created beacause of a legal obligation.']
   ];
 
   const handleScenarioTabChange = (event: React.SyntheticEvent, newValue: number) => {
@@ -489,7 +489,6 @@ function Factsheet(props) {
     const obj = newScenarios.find(el => el.id === idx);
     if (obj)
       obj[name] = selectedList
-    console.log(newScenarios);
     setScenarios(newScenarios);
 
   };
@@ -1078,7 +1077,9 @@ function Factsheet(props) {
 
 
   const handleOpenURL = (e) => {
-    window.open(e, "_blank")
+    if (e !== '') {
+      window.open(e, "_blank")
+    }
   };
 
   const scenarioDescriptorHandler = (descriptorList, nodes, id) => {
@@ -1378,7 +1379,34 @@ function Factsheet(props) {
           <FormGroup>
             <div >
               {
-                StudyKeywords.map((item) => <FormControlLabel control={<Checkbox size="small" color="default" />} checked={selectedStudyKewords.includes(item)} onChange={handleStudyKeywords} label={item} name={item} />)
+                StudyKeywords.map((item) =>
+                  <span >
+                    {item[1] !== '' ? <HtmlTooltip
+                      style={{ marginLeft: '10px' }}
+                      placement="top"
+                      title={
+                        <React.Fragment>
+                          <Typography color="inherit" variant="caption">
+                            {item[2]}
+                            <br />
+                            <a href={item[1]}>More info from Open Enrgy Ontology (OEO)...</a>
+                          </Typography>
+                        </React.Fragment>
+                      }
+                    >
+                      <FormControlLabel control={
+                        <Checkbox size="small" color="default" />
+                      } checked={selectedStudyKewords.includes(item[0])} onChange={handleStudyKeywords} label={item[0]} name={item[0]}
+                      />
+                    </HtmlTooltip>
+                      :
+                      <FormControlLabel control={
+                        <Checkbox size="small" color="default" style={{ marginLeft: "20px" }} />
+                      } checked={selectedStudyKewords.includes(item[0])} onChange={handleStudyKeywords} label={item[0]} name={item[0]}
+                      />
+                    }
+                  </span>
+                )
               }
             </div>
           </FormGroup>
@@ -2304,7 +2332,25 @@ function Factsheet(props) {
               <FormGroup>
                 <div >
                   {
-                    StudyKeywords.map((item) => <FormControlLabel control={<Checkbox size="small" color="default" />} checked={selectedStudyKewords.includes(item)} onChange={handleStudyKeywords} label={item} name={item} />)
+                    StudyKeywords.map((item) => <FormControlLabel control={
+                      <div >
+                        <HtmlTooltip
+                          style={{ marginLeft: '10px' }}
+                          placement="top"
+                          title={
+                            <React.Fragment>
+                              <Typography color="inherit" variant="caption">
+                                {'A sector division is a specific way to subdivide a system.'}
+                                <br />
+                                <a href="http://openenergy-platform.org/ontology/oeo/OEO_00000368">More info from Open Enrgy Ontology (OEO)...</a>
+                              </Typography>
+                            </React.Fragment>
+                          }
+                        >
+                          <Checkbox size="small" color="default" />
+                        </HtmlTooltip>
+                      </div>
+                    } checked={selectedStudyKewords.includes(item)} onChange={handleStudyKeywords} label={item} name={item} />)
                   }
                 </div>
               </FormGroup>
@@ -2765,7 +2811,7 @@ function Factsheet(props) {
                       </Grid>
                       <Grid item xs={9} style={{ paddingTop: '10px' }}>
                         {selectedStudyKewords.map((v, i) => (
-                          <span> <span> {v} </span> <span>   <b className="separator-dot"> . </b></span> </span>
+                          <span> <span> <Chip label={v} size="small" variant="outlined" onClick={() => handleOpenURL(StudyKeywords.filter((i) => i[0] == v)[0][1])} /> </span> <span>   <b className="separator-dot">  </b></span> </span>
                         ))}
                       </Grid>
                     </InfoListItem>
