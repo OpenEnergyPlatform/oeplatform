64d0723e4b71ae7ee9c6e752435f2de42d4a0dee
Jonas Huber <jonas.huber@rl-institut.de>
2024-01-30 18:16:38 +0100

    move utility functionality from views.py

--- api/utilities.py
+++ api/utilities.py
@@ -1,4 +1,7 @@
 from api import actions
+from api.encode import GeneratorJSONEncoder
+from rest_framework import status
+from django.http import StreamingHttpResponse
 
 
 def transform_results(cursor, triggers, trigger_args):
@@ -12,3 +15,26 @@ def transform_results(cursor, triggers, trigger_args):
 
 def conjunction(clauses):
     return {"type": "operator", "operator": "AND", "operands": clauses}
+
+
+class OEPStream(StreamingHttpResponse):
+    def __init__(self, *args, session=None, **kwargs):
+        self.session = session
+        super(OEPStream, self).__init__(*args, **kwargs)
+
+    def __del__(self):
+        if self.session:
+            self.session.close()
+
+
+def stream(data, allow_cors=False, status_code=status.HTTP_200_OK, session=None):
+    encoder = GeneratorJSONEncoder()
+    response = OEPStream(
+        encoder.iterencode(data),
+        content_type="application/json",
+        status=status_code,
+        session=session,
+    )
+    if allow_cors:
+        response["Access-Control-Allow-Origin"] = "*"
+    return response
