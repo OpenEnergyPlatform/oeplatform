311699d1a9efa97c09c1cb1bbb905fffb0bd027e
Jonas Huber <jonas.huber@rl-institut.de>
2024-03-15 12:48:23 +0100

    Add metadata badge utility functionality to get badges from metadata and provide context like the badge icon #1559

--- login/utilities.py
+++ login/utilities.py
@@ -3,12 +3,17 @@ from pathlib import Path
 import logging
 from functools import lru_cache
 import re
+from enum import Enum, auto
+
+from django.templatetags.static import static
 
 from oeplatform.settings import STATIC_ROOT
 
 #####################################################
 # Utilities mainly used for the Tables profile page #
 #####################################################
+
+# Functions below implement the automated license check
 
 
 def normalize_license_name(name):
@@ -83,6 +88,67 @@ def validate_open_data_license(django_table_obj):
         )
 
     return True, None
+
+
+# The following functions implement the retrieval of review badges
+# from the oemetadata and handle the case when no sticker is available.
+
+
+# TODO Refactor this to dataedit app?
+class PeerReviewBadge(Enum):
+    IRON = auto()
+    BRONZE = auto()
+    SILVER = auto()
+    GOLD = auto()
+    PLATINUM = auto()
+
+
+def validate_badge_name_match(badge_name_normalized):
+    matched_badge = None
+    for badge in PeerReviewBadge:
+        if badge_name_normalized == badge.name:
+            matched_badge = badge
+            break
+
+    return matched_badge
+
+
+def get_review_badge_from_table_metadata(django_table_obj):
+    metadata = django_table_obj.oemetadata
+
+    if metadata is None:
+        return False, "Metadata is empty!"
+
+    review = metadata.get("review")
+
+    if not review:
+        return False, "No review information available in the metadata."
+
+    badge = review.get("badge")
+
+    if badge is None and badge != "":
+        return (
+            False,
+            "No badge information available in the metadata.Please start a community-based open peer review for this table first.",
+        )
+
+    badge_name_normalized = badge.upper()
+    badge_check_result = validate_badge_name_match(badge_name_normalized)
+
+    if badge_check_result:
+        return True, badge_check_result.name
+    else:
+        return False, f"No match found for badge name: {badge}"
+
+
+def get_badge_icon_path(badge_name):
+    # Convert the badge name to lowercase and append "_badge.png"
+    normalized_name = f"badge_{badge_name.lower()}.png"
+
+    # Use the Django static function to get the correct static path
+    icon_path = static(f"img/badges/{normalized_name}")
+
+    return icon_path
 
 
 #####################################################
