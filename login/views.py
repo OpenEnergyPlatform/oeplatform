f7197cca2d9ef30c07f3e741896bf1e78418483c
Jonas Huber <jonas.huber@rl-institut.de>
2024-03-15 12:42:25 +0100

    add import #1559

--- login/views.py
+++ login/views.py
@@ -4,6 +4,7 @@ from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView
 from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, View
