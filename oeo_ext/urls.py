3b95cfa32278799dea711618a43873fc7863a90d
Jonas Huber <jonas.huber@rl-institut.de>
2024-05-02 13:27:15 +0200

    setup placeholder view and implement urls for oeo ext form #1679

--- oeo_ext/urls.py
+++ oeo_ext/urls.py
@@ -1,8 +1,14 @@
+from django.conf.urls import url  # noqa:F401
 from django.urls import path  # noqa:F401
 
-from oeo_ext import views  # noqa:F401
+from oeo_ext import views
 
 app_name = "oeo_ext"
 
-
-urlpatterns = []
+urlpatterns = [
+    path(
+        r"oeo-ext-plugin-ui/create",
+        views.OeoExtPluginView.as_view(),
+        name="oeo-ext-plugin-ui-create",
+    )
+]
--- oeo_ext/views.py
+++ oeo_ext/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render  # noqa:F401
+from django.views.generic import View
 
-# Create your views here.
+
+# Suggested views maybe you will use other ones @adel
+class OeoExtPluginView(View, LoginRequiredMixin):
+    def get(self, request):
+        return render(request, "oeo_ext/partials/oeo-ext-plugin-ui.html")
+
+    def post(self, request):
+        return render(request, "oeo_ext/partials/oeo-ext-plugin-ui.html")
