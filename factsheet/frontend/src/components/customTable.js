6012bc62a1fbc197061a01ccc4491a62ae6cda88
Jonas Huber <jonas.huber@rl-institut.de>
2024-06-11 12:59:17 +0200

    revert changes

--- factsheet/frontend/src/components/customTable.js
+++ factsheet/frontend/src/components/customTable.js
@@ -318,8 +318,7 @@ function EnhancedTableToolbar(props) {
               }
             >
               <span>
-                {/* disabled={logged_in === "NOT_LOGGED_IN"} */}
-                <Button  component={Link} variant="contained" size="small" className="linkButton" to={`scenario-bundles/id/new`} onClick={() => this.forceUpdate}>
+                <Button disabled={logged_in === "NOT_LOGGED_IN"} component={Link} variant="contained" size="small" className="linkButton" to={`scenario-bundles/id/new`} onClick={() => this.forceUpdate}>
                   <AddIcon />
                   Create new
                 </Button>
