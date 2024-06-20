26566ac7b18e4daf5706b9b91e1670b426199844
Jonas Huber <jonas.huber@rl-institut.de>
2024-06-20 17:28:09 +0200

    enhance tooltip text

--- factsheet/frontend/src/components/scenarioBundleMicroComponents/factsheetMetadataList.js
+++ factsheet/frontend/src/components/scenarioBundleMicroComponents/factsheetMetadataList.js
@@ -4,7 +4,7 @@ import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
 const FactsheetMetadataList = ({ data }) => {
   return (
     <Box>
-      <Typography variant="h6"><b>{data.acronym}</b> is licensed as {data.license} and maintained by the following institution(s):</Typography>
+      <Typography variant="h6">The factsheet {data.name} with the acronym <b>{data.acronym}</b> is licensed as <b>{data.license}</b> and maintained by the following institution(s):</Typography>
       <List>
         {data.institutions.length > 0 ? (
           data.institutions.map((institution, index) => (
