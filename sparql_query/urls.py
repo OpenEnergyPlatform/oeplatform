7be9decb9f29a717294665b44603387e95b98917
Jonas Huber <jonas.huber@rl-institut.de>
2024-07-18 16:46:32 +0200

    update sparql gui page url path #1754

--- sparql_query/urls.py
+++ sparql_query/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import main_view, sparql_endpoint
 
 urlpatterns = [
-    path("main/", main_view, name="main"),
+    path("gui/", main_view, name="main"),
     path("sparql/", sparql_endpoint, name="sparql_endpoint"),
 ]
