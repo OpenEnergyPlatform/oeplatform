ccd57adf1c9418b62f8ed77d169c8385945c18f4
Adel Memariani <adel.memariani@ovgu.de>
2024-07-19 15:23:19 +0200

    Install Chart.js and add toggle button for quantitative comparison

--- factsheet/frontend/src/components/comparisonBoardMain.js
+++ factsheet/frontend/src/components/comparisonBoardMain.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, } from 'react';
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
 import ComparisonBoardItems from "./comparisonBoardItems";
 // import { Box } from "@mui/system";
 // import ComparisonControl from "./comparisonControl";
@@ -19,6 +21,14 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import BreadcrumbsNavGrid from '../styles/oep-theme/components/breadcrumbsNavigation.js';
 import OptionBox from '../styles/oep-theme/components/optionBox.js';
 // import MultipleSelectChip from '../styles/oep-theme/components/multiselect.js';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+
+
+
+
 
 const ComparisonBoardMain = (props) => {
   const { params } = props;
@@ -26,6 +36,7 @@ const ComparisonBoardMain = (props) => {
   const scenarios_uid = params.split('#');
   const scenarios_uid_json = JSON.stringify(scenarios_uid);
   const [selectedCriteria, setselectedCriteria] = useState(['Study descriptors', 'Scenario types', 'Study name']);
+  const [alignment, setAlignment] = React.useState('Qualitative');
 
   const getScenarios = async () => {
     const { data } = await axios.get(conf.toep + `scenario-bundles/get_scenarios/`, { params: { scenarios_uid: scenarios_uid_json } });
@@ -37,6 +48,14 @@ const ComparisonBoardMain = (props) => {
       setScenarios(data);
     });
   }, []);
+
+  const handleChangeView = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    newAlignment !== null && setAlignment(newAlignment);
+    console.log(alignment);
+  };
 
 
   const Criteria = [
@@ -62,6 +81,79 @@ const ComparisonBoardMain = (props) => {
       setselectedCriteria(filteredCriteria);
     }
   }
+
+    const sampleData = [43, 40, 50, 40, 70, 40, 45, 33, 40, 60, 40, 50, 36];
+  
+    const canvasData = {
+      datasets: [
+        {
+          label: "eu_leg_data_2016_eea",
+          borderColor: "navy",
+          pointRadius: 0,
+          fill: true,
+          backgroundColor: 'lightblue',
+          lineTension: 0.4,
+          data: sampleData,
+          borderWidth: 1,
+          title: 's',
+        },
+      ],
+    };
+  
+    const options = {
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          },
+          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+          ticks: {
+            color: "blue",
+            font: {
+              family: "Nunito",
+              size: 10,
+            },
+          },
+        },
+        y: {
+          grid: {
+            display: false,
+          },
+          border: {
+            display: false,
+          },
+          min: 0,
+          max: 100,
+          ticks: {
+            stepSize: 10,
+            color: "blue",
+            font: {
+              family: "Nunito",
+              size: 12,
+            },
+          },
+        },
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true,
+        },
+        title: {
+          display: true,
+        },
+      },
+    };
+  
+    const graphStyle = {
+      minHeight: "20rem",
+      maxWidth: "100%",
+      width: "100%",
+      border: "1px solid #C4C4C4",
+      borderRadius: "0.375rem",
+      padding: "0.5rem",
+    };
 
   return (
     scenarios.length !== 0 &&
@@ -94,26 +186,55 @@ const ComparisonBoardMain = (props) => {
                 How it works?
               </Button>
             </Grid>
+            <Grid item xs={4}>
+            </Grid>
+            <Grid item xs={6}>
+              <ToggleButtonGroup
+                    color="primary"
+                    value={alignment}
+                    exclusive
+                    onChange={handleChangeView}
+                    aria-label="Platform"
+                    size="large"
+                  >
+                    <ToggleButton style={{ width:'250px' }} value="Qualitative"><ManageSearchIcon />Qualitative</ToggleButton>
+                    <ToggleButton style={{ width:'250px' }} value="Quantitative"><EqualizerIcon />Quantitative</ToggleButton>
+              </ToggleButtonGroup>
+            </Grid>
+            <Grid item xs={2}>
+            </Grid>
           </Grid>
         </Toolbar>
         {/* <ComparisonControl /> */}
-        <OptionBox>
-          <h2>Criteria</h2>
-          <FormGroup>
-            <div >
-              {
-                Criteria.map((item) => <FormControlLabel control={<Checkbox size="medium" color="primary" />} checked={selectedCriteria.includes(item)} onChange={handleCriteria} label={item} name={item} />)
-              }
-            </div>
-          </FormGroup>
-          {/* <MultipleSelectChip
-            sx={{ mt: 2, width: "100%" }}
-            options={['Scenario 1', 'Scenario 2', 'Scenario 3']}
-            label="Scenarios to be compared"
-            disabled={true}
-          /> */}
-        </OptionBox>
-        <ComparisonBoardItems elements={scenarios} c_aspects={selectedCriteria} />
+
+        {alignment == "Qualitative" && 
+        <Grid item xs={12}>
+          <OptionBox>
+            <h2>Criteria</h2>
+            <FormGroup>
+              <div >
+                {
+                  Criteria.map((item) => <FormControlLabel control={<Checkbox size="medium" color="primary" />} checked={selectedCriteria.includes(item)} onChange={handleCriteria} label={item} name={item} />)
+                }
+              </div>
+            </FormGroup>
+            {/* <MultipleSelectChip
+              sx={{ mt: 2, width: "100%" }}
+              options={['Scenario 1', 'Scenario 2', 'Scenario 3']}
+              label="Scenarios to be compared"
+              disabled={true}
+            /> */}
+          </OptionBox>
+          <ComparisonBoardItems elements={scenarios} c_aspects={selectedCriteria} />
+        </Grid>
+        } 
+        {alignment == "Quantitative" && 
+        <Grid item xs={12}>
+          <div style={graphStyle}>
+            <Line id="home" options={options} data={canvasData} />
+          </div>
+        </Grid>
+        } 
       </Container>
     </Grid>
   );
