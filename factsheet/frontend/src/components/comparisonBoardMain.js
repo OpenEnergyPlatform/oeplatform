eaf216920404654e800cc38459d5ad3b2a6b976e
Adel Memariani <adel.memariani@ovgu.de>
2024-07-23 12:29:34 +0200

    Connect frontend to OBDI backend #1765

--- factsheet/frontend/src/components/comparisonBoardMain.js
+++ factsheet/frontend/src/components/comparisonBoardMain.js
@@ -16,7 +16,7 @@ import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Toolbar from '@mui/material/Toolbar';
-import { Tooltip } from '@mui/material';
+import { Tooltip, Box } from '@mui/material';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import BreadcrumbsNavGrid from '../styles/oep-theme/components/breadcrumbsNavigation.js';
 import OptionBox from '../styles/oep-theme/components/optionBox.js';
@@ -26,17 +26,32 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
 import Typography from '@mui/material/Typography';
-
-
-
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import 'chartjs-plugin-datalabels'
+import CSRFToken from './csrfToken.js';
+import CircularProgress from '@mui/material/CircularProgress';
+import SendIcon from '@mui/icons-material/Send';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const ComparisonBoardMain = (props) => {
+
   const { params } = props;
   const [scenarios, setScenarios] = useState([]);
   const scenarios_uid = params.split('#');
   const scenarios_uid_json = JSON.stringify(scenarios_uid);
   const [selectedCriteria, setselectedCriteria] = useState(['Study descriptors', 'Scenario types', 'Study name']);
   const [alignment, setAlignment] = React.useState('Qualitative');
+  const [sparqOutput, setSparqlOutput] = useState('');
+  const [scenarioYear, setScenarioYear] = React.useState('2020');
+  const [chartData, setChartData] = React.useState([]);
+  const [chartLabels, setChartLabels] = React.useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showChart, setShowChart] = useState(false);
+
+  
 
   const getScenarios = async () => {
     const { data } = await axios.get(conf.toep + `scenario-bundles/get_scenarios/`, { params: { scenarios_uid: scenarios_uid_json } });
@@ -54,9 +69,10 @@ const ComparisonBoardMain = (props) => {
     newAlignment: string,
   ) => {
     newAlignment !== null && setAlignment(newAlignment);
-    console.log(alignment);
   };
 
+  
+  // 'http://oevkg:8080/sparql'
 
   const Criteria = [
     'Scenario abstract',
@@ -82,8 +98,72 @@ const ComparisonBoardMain = (props) => {
     }
   }
 
-  const dataBar = {
-    labels: ["AT","BE","CY","CZ","DE","DK","EE","ES","FI","FR","GB","GR","HR","HU","IE","IS","IT","LT","LV","MT","NL","PL","PT","SE","SI","SK"],
+  const handleYearChange = (event) => {
+    setScenarioYear(event.target.value);
+    const filtered_output = sparqOutput.filter(item => item.year.value == event.target.value)
+    const country = filtered_output.map((obj) => obj.country_code.value.split('/').pop() );
+    const value = filtered_output.map((obj) => obj.value.value );
+    setChartData(value);
+    setChartLabels(country);
+    
+  };
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const data_tabels = `"scenario_eu_leg_data_2021"`;
+  const scenario_years =  `"2020", "2025", "2030", "2035", "2040"`;
+  const categories =  `"1.A.3 Transport"`;
+
+  const query = `PREFIX obo: <http://purl.obolibrary.org/obo/>
+  PREFIX ou: <http://opendata.unex.es/def/ontouniversidad#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX oeo: <http://openenergy-platform.org/ontology/oeo/>
+  PREFIX llc:  <https://www.omg.org/spec/LCC/Countries/ISO3166-1-CountryCodes/>
+  
+  SELECT DISTINCT ?table_name ?value ?unit ?country_code ?year ?category WHERE {
+    ?s oeo:OEO_00000504 ?table_name .
+    ?s oeo:OEO_00040010 ?unit .
+    ?s oeo:IAO_0000136 oeo:OEO_00000025.
+    ?s oeo:OEO_00020226 oeo:OEO_00020310 .
+    ?s oeo:has_sector_division ?category .
+    ?s oeo:OEO_00140178 ?value .
+    ?s oeo:OEO_00010378 ?country_code .
+    ?s oeo:OEO_00020224 ?year .
+    FILTER(?year IN (${scenario_years}) && ?table_name IN (${data_tabels}) && ?category IN (${categories})) .
+  } ORDER BY Asc(?year)`;
+
+  const sendQuery = async () => {
+    setLoading(true);
+    const response = await axios.post(
+      'http://localhost:8080/sparql',  
+      query,
+      {
+        headers: {
+          'X-CSRFToken': CSRFToken(),
+          'Accept': 'application/sparql-results+json',
+          'Content-Type': 'application/sparql-query',
+        },
+      }
+    ).then(response => {
+      const sparqOutput = response.data.results.bindings;
+      setSparqlOutput(sparqOutput);
+      const filtered_output = sparqOutput.filter(item => item.year.value == scenarioYear)
+      const country = filtered_output.map((obj) => obj.country_code.value.split('/').pop() );
+      const value = filtered_output.map((obj) => obj.value.value );
+      setChartData(value);
+      setChartLabels(country);
+      setLoading(false);
+      setShowChart(true);
+    }).catch(error => {
+        console.error('API Error:', error.message);
+    }).finally(() => {
+    });
+  }
+
+  const dataBar1 = {
+    labels: chartLabels,
     datasets: [
       {
         label: "eu_leg_data_2016_eea",
@@ -92,7 +172,7 @@ const ComparisonBoardMain = (props) => {
         borderWidth: 1,
         hoverBackgroundColor: "rgba(255,99,132,0.4)",
         hoverBorderColor: "rgba(255,99,132,1)",
-        data: [79,113,8.6,126,835,43,19,331,52,461,414,89,23,63,61,5,419,21,11,2,169,398,63,49,17,42]
+        data: chartData
       }
     ]
   };
@@ -103,14 +183,23 @@ const ComparisonBoardMain = (props) => {
         display: true,
         color: "black",
         formatter: Math.round,
-        anchor: "end",
-        offset: -20,
-        align: "start"
+        font: {
+          weight: 'bold'
+        },
+        align: "top",
+        anchor: "end"
+      },
+      legend: {
+        display: false
+      },
+      tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
+        }
       }
     },
-    legend: {
-      display: false
-    }
   };
 
   return (
@@ -187,19 +276,48 @@ const ComparisonBoardMain = (props) => {
         </Grid>
         } 
         {alignment == "Quantitative" && 
-        <Grid item xs={12}>
-          <Typography variant="h6" gutterBottom>
-            with existing measures scenario' submitted in 2019
-          </Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            This visualisation depicts the projected 2020 - 2035 data from the WEM scenario submissions made by European Member States to the European Commission in 2019. 
-          </Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            The data displayed refers to total greenhouse gas emissions excluding LULUCF and excluding international aviation. 
-          </Typography>
-          <div>
-            <Bar data={dataBar} options={options} width={100} height={40} />
-          </div>
+        <Grid container >
+          <Grid item xs={11}>
+            
+          </Grid>
+          <Grid item xs={1}>
+            <Button variant="outlined" endIcon={<SendIcon />} onClick={sendQuery} >Go</Button>
+          </Grid>
+          <Grid item xs={12}>
+            {loading == true && <Box sx={{ paddingTop: "10px" }}>
+              <LinearProgress />
+            </Box>}
+            {showChart == true && <Grid container>
+              <Grid item xs={12}>
+                <FormControl fullWidth margin="dense">
+                  <FormLabel ></FormLabel>
+                  <RadioGroup
+                    row
+                    name="row-radio-buttons-group"
+                    onChange={handleYearChange}
+                    defaultValue="2020"
+                  >
+                    <Box sx={{ 
+                      display: 'flex', 
+                      paddingLeft: '20px',
+                      paddingRight: '20px',
+                      justifyContent: 'space-between',
+                      width: '100%'
+                    }}>
+                      <FormControlLabel value="2020" control={<Radio />} label="2020" />
+                      <FormControlLabel value="2025" control={<Radio />} label="2025" />
+                      <FormControlLabel value="2030" control={<Radio />} label="2030" />
+                      <FormControlLabel value="2035" control={<Radio />} label="2035" />
+                      <FormControlLabel value="2040" control={<Radio />} label="2040" />
+                    </Box>
+                    </RadioGroup>
+                  </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Bar data={dataBar1} options={options} width={100} height={40} />
+              </Grid>
+            </Grid>}
+          </Grid>
         </Grid>
         } 
       </Container>
