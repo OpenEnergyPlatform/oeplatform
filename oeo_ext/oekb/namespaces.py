fd66d18a3953d1f9f9851e265fc3b7cee5cb7e79
Jonas Huber <jonas.huber@rl-institut.de>
2024-08-02 13:00:52 +0200

    Add oeo extended to namespaces #1679

--- oeo_ext/oekb/namespaces.py
+++ oeo_ext/oekb/namespaces.py
@@ -2,6 +2,7 @@ from rdflib.namespace import Namespace
 
 # TODO- Alot of hardcoded URL, transfer to settings or other config
 OEO = Namespace("http://openenergy-platform.org/ontology/oeo/")
+OEOX = Namespace("http://openenergy-platform.org/ontology/oeox/")
 OBO = Namespace("http://purl.obolibrary.org/obo/")
 DC = Namespace("http://purl.org/dc/terms/")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -13,6 +14,7 @@ DBO = Namespace("http://dbpedia.org/ontology/")
 
 def bind_all_namespaces(graph):
     graph.bind("OEO", OEO)
+    graph.bind("OEOX", OEOX)
     graph.bind("OBO", OBO)
     graph.bind("DC", DC)
     graph.bind("RDFS", RDFS)
