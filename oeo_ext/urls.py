525140724bba690e93fbbeeb5685fc754ff4ee14
Jonas Huber <jonas.huber@rl-institut.de>
2024-08-02 12:50:26 +0200

    add path for unit element #1679

--- oeo_ext/urls.py
+++ oeo_ext/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
         r"oeo-ext-plugin-ui/create",
         views.OeoExtPluginView.as_view(),
         name="oeo-ext-plugin-ui-create",
-    )
+    ),
+    path("add-unit-element/", views.add_unit_element, name="add_unit_element"),
+    # path("search-oeo-units/", views.search_units, name="search_oeo_unit"),
 ]
