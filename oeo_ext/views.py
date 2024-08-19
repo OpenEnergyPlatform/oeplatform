024bbdb862fd246ca44e7fbfe369372aab18208b
Jonas Huber <jonas.huber@rl-institut.de>
2024-08-19 19:20:59 +0200

    update view to handle new forms & process the user data from form #1712

--- oeo_ext/views.py
+++ oeo_ext/views.py
@@ -1,36 +1,12 @@
-import types
+import json
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.shortcuts import render  # noqa:F401
 from django.views.generic import View
-from owlready2 import get_ontology
-from rdflib import Graph
 
-from oeo_ext.oekb.connection import oeo_owl
-from oeo_ext.utils import get_new_iri
-from oeplatform.settings import ONTOLOGY_ROOT
-
-OEO_EXT_PATH = ONTOLOGY_ROOT / "oeo_ext/oeo_ext.owl"
-
-# load oeo via common interface
-oeo = oeo_owl
-
-oeo_ext = Graph()
-oeo_ext.parse(OEO_EXT_PATH.as_uri())
-oeo_ext_owl = get_ontology(OEO_EXT_PATH.as_posix()).load()
-
-# oeo_ext.base_iri = "http://openenergy-platform.org/ontology/oeo/oeo_ext/oeo_ext.owl#"
-
-# Static parts, object properties:
-has_linear_unit_numerator = oeo.search_one(label="has unit numerator")
-has_squared_unit_numerator = oeo.search_one(label="has squared unit numerator")
-has_cubed_unit_numerator = oeo.search_one(label="has cubed unit numerator")
-has_linear_unit_denominator = oeo.search_one(label="has linear unit denominator")
-has_squared_unit_denominator = oeo.search_one(label="has squared unit denominator")
-has_cubed_unit_denominator = oeo.search_one(label="has cubed unit denominator")
-unit = oeo.search_one(label="unit")
-
-UNITS = oeo.search(label="unit")
+from oeo_ext.forms import ComposedUnitFormWrapper, UnitEntryForm
+from oeo_ext.utils import create_new_unit
 
 
 # Suggested views maybe you will use other ones @adel
@@ -40,37 +16,65 @@ class OeoExtPluginView(View, LoginRequiredMixin):
     """
 
     def get(self, request):
-        print(request)
-
-        return render(request, "oeo_ext/partials/oeo-ext-plugin-ui.html")
+        form = ComposedUnitFormWrapper()
+        context = {
+            "form": form,
+            "nominator_forms": [],
+            "denominator_forms": [],
+        }
+        return render(request, "oeo_ext/partials/oeo-ext-plugin-ui.html", context)
 
     def post(self, request):
-        print("post")
+        try:
+            data = json.loads(request.POST.get("data", None))
+            form = ComposedUnitFormWrapper(data)
 
-        new_class_iri = get_new_iri(ontox=oeo_ext_owl)
-        label_of_unit, classesUsed = None, {}
-        lists = list(classesUsed.keys())
-        class_definition = "( "
-        for i in range(0, len(lists)):
-            for units_used in classesUsed[lists[i]]:
-                locals()[units_used] = oeo.search_one(label=units_used)
-                class_definition += (
-                    "has_" + str(lists[i])[:-1] + ".some(" + units_used + ") &"
-                )
-        class_definition = class_definition[:-1]
-        class_definition += " )"
+            nominator_data = data.get("nominator", [])
+            denominator_data = data.get("denominator", [])
 
-        with oeo_ext_owl:
-            NewClass = types.new_class(new_class_iri, (UNITS,))
-            NewClass.label = str(label_of_unit)
-            NewClass.equivalent_to = [eval(class_definition)]
-        oeo_ext_owl.save(file=OEO_EXT_PATH, format="rdfxml")
+            nominator_forms = [UnitEntryForm(item) for item in nominator_data]
+            denominator_forms = [UnitEntryForm(item) for item in denominator_data]
 
-        return None
+            all_forms_valid = (
+                form.is_valid()
+                and all(f.is_valid() for f in nominator_forms)
+                and all(f.is_valid() for f in denominator_forms)
+            )
+
+            if all_forms_valid:
+                data = {
+                    "success": True,
+                    "message": "Form data is valid",
+                    "data": {
+                        "definition": form.cleaned_data.get("definition"),
+                        "unitLabel": form.cleaned_data.get("unitLabel"),
+                        "nominator": [f.cleaned_data for f in nominator_forms],
+                        "denominator": [f.cleaned_data for f in denominator_forms],
+                    },
+                }
+                n = data.get("data", {})["nominator"]
+                d = data.get("data", {})["denominator"]
+                # new_unit_uir is either URIRef type or None
+                # error is either dict with key:value or None
+                new_unit, error = create_new_unit(numerator=n, denominator=d)
+
+                response_data = data
+                return JsonResponse(response_data, status=200)
+            else:
+                errors = {
+                    "form": form,
+                    "form_errors": form.errors,
+                    "nominator_errors": [f.errors for f in nominator_forms],
+                    "denominator_errors": [f.errors for f in denominator_forms],
+                }
+                response_data = {"success": False, "errors": errors}
+                return JsonResponse(response_data, status=400)
+        except json.JSONDecodeError as e:
+            response_data = {"success": False, "errors": {"json_error": str(e)}}
+            return JsonResponse(response_data, status=400)
 
 
 def add_unit_element(request):
-    # unit_id = randint(0, 1000000)
     unit_id = None
     unit_id = request.session.get("unit_id_counter", 0)
     # Increment the counter
@@ -84,6 +88,8 @@ def add_unit_element(request):
 
 
 # def search_units(request):
+# can only be implemented if there is a way to get all classes form the oeo
+# that are a unit
 #     query = request.GET.get("query", "")
 #     results = []
 #     if query:
