e94e5d01d4d2e5a234784ed6a176d32cd18e07b2
Jonas Huber <jonas.huber@rl-institut.de>
2024-08-21 23:42:56 +0200

    add test setup #1712

--- oeo_ext/tests.py
+++ oeo_ext/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase  # noqa:F401
 
 # Create your tests here.
+
+
+class OeoxNewUnitTestCase(TestCase):
+    def setUp(self):
+        self.mock_user_data_struct = {
+            "nominator": [
+                {
+                    "position": 1,
+                    "unitName": "test data set",
+                    "unitType": "linear",
+                    "unitPrefix": "text file format",
+                }
+            ],
+            "denominator": [
+                {
+                    "position": 1,
+                    "unitName": "legal name",
+                    "unitType": "linear",
+                    "unitPrefix": "",
+                }
+            ],
+            "definition": None,
+            "unitLabel": None,
+        }
+
+    def test_create_new_oeox_unit(self):
+        pass
