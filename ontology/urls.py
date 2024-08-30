d421e139757a333c5f18e34720808b2769c2297d
Jonas Huber <jonas.huber@rl-institut.de>
2024-08-30 14:06:41 +0200

    update and enhance ontology urls.py

--- ontology/urls.py
+++ ontology/urls.py
@@ -4,18 +4,17 @@ from django.views.generic import TemplateView
 from ontology import views
 
 urlpatterns = [
-    url(r"^$", views.OntologyVersion.as_view()),
+    url(r"^$", views.OntologyAbout.as_view()),
     url(
         "partial/page-content/",
-        views.PartialOntologyOverviewContent.as_view(),
+        views.PartialOntologyAboutContent.as_view(),
         name="partial-page-content",
     ),
     url(
         "partial/page-sidebar-content/",
-        views.PartialOntologyOverviewSidebarContent.as_view(),
+        views.PartialOntologyAboutSidebarContent.as_view(),
         name="partial-page-sidebar-content",
     ),
-    url(r"^ontology/$", views.OntologyVersion.as_view()),
     url(
         r"^oeo-steering-committee/$",
         TemplateView.as_view(template_name="ontology/oeo-steering-committee.html"),
@@ -24,6 +23,18 @@ urlpatterns = [
     url(
         r"^ontology/oeo-steering-committee/$",
         TemplateView.as_view(template_name="ontology/oeo-steering-committee.html"),
+    ),
+    url(
+        r"^(?P<ontology>[\w_-]+)\/releases/latest?$",
+        views.OntologyStatics.as_view(),
+        {"full": True},
+        name="oeo-latest-full-zip",
+    ),
+    url(
+        r"^(?P<ontology>[\w_-]+)\/releases/latest/glossary?$",
+        views.OntologyStatics.as_view(),
+        {"glossary": True},
+        name="oeo-latest-glossary",
     ),
     url(
         r"^(?P<ontology>[\w_-]+)\/releases(\/v?(?P<version>[\d\.]+))?\/imports\/(?P<file>[\w_-]+)(.(?P<extension>[\w_-]+))?$",  # noqa
@@ -38,6 +49,11 @@ urlpatterns = [
         r"^(?P<ontology>[\w_-]+)\/dev\/(?P<file>[\w_-]+)(.(?P<extension>[\w_-]+))?$",
         views.OntologyStatics.as_view(),
     ),
+    url(
+        r"^(?P<ontology>[\w_-]+)?/$",
+        views.OntologyStatics.as_view(),
+        name="oeo-initializer",
+    ),
     # url(
     #     r"^(?P<ontology>[\w_-]+)\/imports\/(?P<module_or_id>[\w\d_-]+)",
     #     views.OntologyOverview.as_view(),
@@ -47,10 +63,5 @@ urlpatterns = [
         r"^(?P<ontology>[\w_-]+)?/(?P<module_or_id>[\w\d_-]+)?/$",
         views.OntologyViewClasses.as_view(),
         name="oeo-classes",
-    ),
-    url(
-        r"^(?P<ontology>[\w_-]+)?/$",
-        views.OntologyOverview.as_view(),
-        name="oeo-initializer",
     ),
 ]
