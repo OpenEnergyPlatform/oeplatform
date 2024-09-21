d7ea79ed2380825d00e797b1129ac2b0fad845ea
Adel Memariani <adel.memariani@ovgu.de>
2024-09-21 19:45:29 +0200

    Remove static data #1765

--- factsheet/frontend/src/components/comparisonBoardMain.js
+++ factsheet/frontend/src/components/comparisonBoardMain.js
@@ -561,8 +561,8 @@ const ComparisonBoardMain = (props) => {
   const sendGetCategoriesQuery = async () => {
     setLoading(true);
 
-    // const data_tabels = [];
-    const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
+    const data_tabels = [];
+    // const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
     // const data_tabels = [`"eu_leg_data_2023_eea"`] ;
 
     selectedOutputDatasets.map(elem  => data_tabels.push('"' + elem.split(":")[1] + '"'));
@@ -626,9 +626,9 @@ const ComparisonBoardMain = (props) => {
 
   const sendGetGasQuery = async () => {
     setLoading(true);
-    const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
+    // const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
     // const data_tabels = [`"eu_leg_data_2023_eea"`] ;
-    // const data_tabels = [] ;
+    const data_tabels = [] ;
 
     selectedOutputDatasets.map(elem  => data_tabels.push('"' + elem.split(":")[1] + '"'));
 
@@ -751,9 +751,9 @@ const sendQuery = async (index) => {
     setChartData([]);
     setLoading(true);
 
-    const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
+    // const data_tabels = [`"eu_leg_data_2023_eea"`, `"scenario_eu_leg_data_2021"`] ;
     // const data_tabels = [ `"scenario_eu_leg_data_2021"`] ;
-    // const data_tabels = [];
+    const data_tabels = [];
 
     selectedInputDatasets.map(elem  => data_tabels.push('"' + elem.split(":")[1] + '"'));
     selectedOutputDatasets.map(elem  => data_tabels.push('"' + elem.split(":")[1] + '"'));
