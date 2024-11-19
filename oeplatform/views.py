6194224e1ccfed1790553a63ac8d9fdf627e7f17
jh-RLI <jonas.huber@rl-institut.de>
2024-11-19 11:51:12 +0100

    add view to populate user name in case of social login

--- oeplatform/views.py
+++ oeplatform/views.py
@@ -1,3 +1,5 @@
+from allauth.socialaccount.signals import pre_social_login
+from django.dispatch import receiver
 from django.shortcuts import redirect
 from django.views import View
 
@@ -12,3 +14,11 @@ class ImagesView(View):
 def redirect_tutorial(request):
     """all old links totutorials: redirect to new (external) page"""
     return redirect(settings.EXTERNAL_URLS["tutorials_index"])
+
+
+@receiver(pre_social_login)
+def populate_profile(request, sociallogin, **kwargs):
+    social_account = sociallogin.account
+    user = request.user
+    user.name = social_account.name
+    user.save()
