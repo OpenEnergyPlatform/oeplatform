7d20e2915b420d4fe49de64f3757b46ed37ae1e0
jh-RLI <jonas.huber@rl-institut.de>
2024-11-27 15:36:17 +0100

    add various django allauth templates and remove custom login middleware #1363 #1895

--- oeplatform/settings.py
+++ oeplatform/settings.py
@@ -78,7 +78,6 @@ MIDDLEWARE = (
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "login.middleware.DetachMiddleware",
     "axes.middleware.AxesMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -243,22 +242,27 @@ COMPRESS_MTIME_DELAY = 0
 ACCOUNT_USER_MODEL_USERNAME_FIELD = "name"
 ACCOUNT_USER_MODEL_EMAIL_FIELD = "email"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-# ACCOUNT_EMAIL_VERIFICATION = "mandatory" # requires ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # requires ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "login.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/forms.html
 ACCOUNT_FORMS = {"signup": "login.forms.CreateUserForm"}
-# ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_ALLOW_REGISTRATION = True
+ACCOUNT_FORMS = {"signup": "login.forms.CreateUserForm"}
+# ACCOUNT_SIGNUP_FORM_CLASS = {"login.forms.CreateUserForm"}
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 
 
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 SOCIALACCOUNT_ADAPTER = "login.adapters.SocialAccountAdapter"
+SOCIALACCOUNT_EMAIL_VERIFICATION = "optional"
 # https://django-allauth.readthedocs.io/en/latest/forms.html
-SOCIALACCOUNT_FORMS = {"signup": "htmxdj.users.forms.UserSocialSignupForm"}
-
+# SOCIALACCOUNT_FORMS = {"signup": "login.forms.CreateUserForm"}
+SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
+SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = True
 
 # axes login throttling
 AXES_ENABLED = True
