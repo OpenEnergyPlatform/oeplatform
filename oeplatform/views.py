95461583cc0bfe93b70cd00bda2b46fd93474f93
Alexandro Steinert <alexandro.steinert@offis.de>
2024-11-27 17:50:36 +0100

    Added some basic missing templates for sociallogin workflow

--- oeplatform/views.py
+++ oeplatform/views.py
@@ -16,10 +16,3 @@ def redirect_tutorial(request):
     """all old links totutorials: redirect to new (external) page"""
     return redirect(settings.EXTERNAL_URLS["tutorials_index"])
 
-
-@receiver(pre_social_login)
-def populate_profile(request, sociallogin, **kwargs):
-    social_account = sociallogin.account
-    user = request.user
-    user.name = social_account.name
-    user.save()
