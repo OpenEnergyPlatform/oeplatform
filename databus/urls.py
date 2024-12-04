b3ad6e9097af8db257c978a506372373d91e4b28
jh-RLI <jonas.huber@rl-institut.de>
2024-12-04 11:19:13 +0100

    add main route to trigger the registration of a dataset on the databus #1348

--- databus/urls.py
+++ databus/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, re_path  # noqa:F401
 
-from oeo_ext import views  # noqa:F401
+from databus import views  # noqa:F401
 
 app_name = "databus"
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "distribution/table/<str:schema>/<str:table>/register/",
+        views.DatabusRegister.as_view(),
+        name="register",
+    )
+]
