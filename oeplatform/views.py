a82b2ed784466f26ae4fce22b6ad768bd7fb7efc
jh-RLI <jonas.huber@rl-institut.de>
2024-12-05 11:05:57 +0100

    make linter happy

--- oeplatform/views.py
+++ oeplatform/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import redirect
 from django.views import View
 
 from oeplatform import settings
-from django.dispatch import receiver
-from allauth.socialaccount.signals import pre_social_login
-from allauth.socialaccount.models import SocialAccount
-from login.models import myuser
+
 
 class ImagesView(View):
     def get(self, request, f):
@@ -15,4 +12,3 @@ class ImagesView(View):
 def redirect_tutorial(request):
     """all old links totutorials: redirect to new (external) page"""
     return redirect(settings.EXTERNAL_URLS["tutorials_index"])
-
