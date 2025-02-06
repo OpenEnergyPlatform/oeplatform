36cbb58b52e02da012abbb2d4c76e5d846ab4630
jh-RLI <jonas.huber@rl-institut.de>
2025-02-06 17:33:20 +0100

    add dataset UUID field to config dataclass and implement method to read the config from serialized user data

--- oekg/sparqlModels.py
+++ oekg/sparqlModels.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from uuid import UUID
+from uuid import UUID, uuid4
 
 
 @dataclass
@@ -8,6 +8,17 @@ class DatasetConfig:
     scenario_uuid: UUID
     dataset_label: str
     dataset_url: str
-    dataset_id: int
-    dataset_key: bool
+    dataset_id: UUID
     dataset_type: str
+
+    @classmethod
+    def from_serializer_data(cls, validated_data: dict, dataset_entry: dict):
+        """Converts validated serializer data into a DatasetConfig object."""
+        return cls(
+            bundle_uuid=validated_data["scenario_bundle"],
+            scenario_uuid=validated_data["scenario"],
+            dataset_label=dataset_entry["name"],
+            dataset_url=dataset_entry["external_url"],
+            dataset_id=uuid4(),
+            dataset_type=dataset_entry["type"],  # "input" or "output"
+        )
