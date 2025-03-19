2945d7b74bfce959570fcda95cf30a358ae4e499
vismayajochem <vismayajochem>
2025-03-19 11:07:22 +0100

    Add licenses #1944

--- base/static/js/activate-bootstrap-popover.js
+++ base/static/js/activate-bootstrap-popover.js
@@ -1,3 +1,9 @@
+/*
+* SPDX-FileCopyrightText: 2020 Jonas Huber <jh-RLI> © Reiner Lemoine Institut
+* SPDX-FileCopyrightText: oeplatform <https://github.com/OpenEnergyPlatform/oeplatform/>
+* SPDX-License-Identifier: MIT
+*/
+
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
