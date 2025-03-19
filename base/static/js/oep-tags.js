d92a632d0ca99b27bd11aafcaa0bdd65e65ba43d
vismayajochem <vismayajochem>
2025-03-19 11:21:51 +0100

    Add licenses #1944

--- base/static/js/oep-tags.js
+++ base/static/js/oep-tags.js
@@ -1,3 +1,10 @@
+/*! 
+* SPDX-FileCopyrightText: 2020 Johann Wagner <johannwagner>
+* SPDX-FileCopyrightText: 2022 Christian Winger <wingechr>
+* SPDX-FileCopyrightText: oeplatform <https://github.com/OpenEnergyPlatform/oeplatform/>
+* SPDX-License-Identifier: MIT
+*/
+
 $(".tag-checkbox").on('click', (e) => {
   $(e.target.parentElement).toggleClass("tag-checkbox-checked", e.target.checked);
 });
