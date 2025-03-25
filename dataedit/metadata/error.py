df6e75bfb10d183f88fdd3aa00085340bbf29d3e
vismayajochem <vismayajochem>
2025-03-25 14:26:56 +0100

    Add licenses #1944

--- dataedit/metadata/error.py
+++ dataedit/metadata/error.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017 Martin Glauer <MGlauer>
+# SPDX-FileCopyrightText: oeplatform <https://github.com/OpenEnergyPlatform/oeplatform/>
+# SPDX-License-Identifier: MIT
+
 class MetadataException(Exception):
     def __init__(self, metadata, error):
         self.metadata = metadata
