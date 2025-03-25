e72435e5170d1ff6374a4290c2465b5c9fa7eb85
vismayajochem <vismayajochem>
2025-03-25 12:31:37 +0100

    Add license #1944

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2018 Martin Glauer <MGlauer>
+# SPDX-FileCopyrightText: oeplatform <https://github.com/OpenEnergyPlatform/oeplatform/>
+# SPDX-License-Identifier: MIT
+
 from django import template
 
 register = template.Library()
