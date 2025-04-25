73df50098d20462b48666f2156af88a71192b9f6
jh-RLI <jonas.huber@rl-institut.de>
2025-04-25 15:21:05 +0200

    #1970: update vite config to include eslint plugin

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -12,6 +13,11 @@ export default defineConfig({
       babel: {
         plugins: ['@emotion/babel-plugin'],
       },
+    }),
+    eslint({
+      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+      failOnWarning: false,
+      failOnError:   true,
     }),
   ],
   optimizeDeps: {
