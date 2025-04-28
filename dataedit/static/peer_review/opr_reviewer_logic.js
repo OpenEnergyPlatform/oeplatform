613b1d9444eeeddb8ce19cfaa6786dad2c252a16
Darynarli <Daryna.Barabanova@rl-institut.de>
2025-04-28 18:13:37 +0200

    new file with reviewer logic for importing #1963

---
+++ dataedit/static/peer_review/opr_reviewer_logic.js
@@ -0,0 +1,71 @@
+import {current_review, getAllFieldsAndValues, getFieldState, isEmptyValue, showToast, sendJson} from "./peer_review.js";
+export function finishPeerReview() {
+  $('#peer_review-submitting').removeClass('d-none');
+
+  var selectedBadge = $('input[name="reviewer-option"]:checked').val();
+  console.log(selectedBadge);
+  current_review.badge = selectedBadge;
+  current_review.reviewFinished = true;
+  let json = JSON.stringify({reviewType: 'finished', reviewData: current_review, reviewBadge: selectedBadge});
+  sendJson("POST", config.url_peer_review, json).then(function() {
+    window.location = config.url_table;
+  }).catch(function(err) {
+    // TODO evaluate error, show user message
+    $('#peer_review-submitting').addClass('d-none');
+    alert(getErrorMsg(err));
+  });
+}
+export function check_if_review_finished() {
+    if (!checkFieldStates()) {
+        return;
+    }
+
+    if (!clientSideReviewFinished) {
+        clientSideReviewFinished = true;
+        showToast("Review completed!", "You completed the review and can now award a suitable badge!", 'success');
+
+        var reviewerDiv = $('<div class="bg-warning" id="finish-review-div"></div>');
+        var bronzeRadio = $('<input type="radio" name="reviewer-option" value="bronze"> Bronze<br>');
+        var silverRadio = $('<input type="radio" name="reviewer-option" value="silver"> Silver<br>');
+        var goldRadio = $('<input type="radio" name="reviewer-option" value="gold"> Gold<br>');
+        var platinRadio = $('<input type="radio" name="reviewer-option" value="platin"> Platin <br>');
+        var reviewText = $('<p>The review is complete. Please award a badge and finish the review.</p>');
+        var finishButton = $('<button type="button" id="review-finish-button">Finish</button>');
+
+        reviewerDiv.append(reviewText);
+        reviewerDiv.append(bronzeRadio);
+        reviewerDiv.append(silverRadio);
+        reviewerDiv.append(goldRadio);
+        reviewerDiv.append(platinRadio);
+        reviewerDiv.append(finishButton);
+
+        finishButton.on('click', finishPeerReview);
+
+        if (!config.review_finished) {
+            reviewerDiv.show();
+            $('#submit_summary').prop('disabled', true);
+        } else {
+            reviewerDiv.hide();
+            $('#submit_summary').hide();
+            $('#peer_review-save').hide();
+            $('#review-window').css('visibility', 'hidden');
+        }
+
+        $('.content-finish-review').append(reviewerDiv);
+    }
+}
+
+export function checkFieldStates() {
+    const allFields = getAllFieldsAndValues();
+
+    for (const { fieldName, fieldValue } of allFields) {
+        if (!isEmptyValue(fieldValue)) {
+            const fieldState = getFieldState(fieldName);
+
+            if (fieldState !== 'ok' && fieldState !== 'rejected') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
