95d0daf4d6c22be2c2fb12ec59cdaa7609714b38
jh-RLI <jonas.huber@rl-institut.de>
2025-07-10 10:35:49 +0200

    update oeo extended base IRI

--- oeo_ext/oeo_extended_store/namespaces.py
+++ oeo_ext/oeo_extended_store/namespaces.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 Jonas Huber <https://github.com/jh-RLI> © Reiner Lemoine Institut
+# SPDX-FileCopyrightText: 2025 Jonas Huber <https://github.com/jh-RLI> © Reiner Lemoine Institut # noqa
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -6,7 +6,7 @@ from rdflib.namespace import OWL, RDF, XSD, Namespace
 
 # TODO- Alot of hardcoded URL, transfer to settings or other config
 OEO = Namespace("https://openenergyplatform.org/ontology/oeo/")
-OEOX = Namespace("http://openenergy-platform.org/ontology/oeox/")
+OEOX = Namespace("https://openenergyplatform.org/ontology/oeox/")
 OBO = Namespace("http://purl.obolibrary.org/obo/")
 DC = Namespace("http://purl.org/dc/terms/")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
