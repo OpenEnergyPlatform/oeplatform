05793b70793cda87b6ca024c55bed25cc56bd2cf
Darynarli <Daryna.Barabanova@rl-institut.de>
2025-07-16 13:50:46 +0200

    remove versions #1941

--- ontology/utils.py
+++ ontology/utils.py
@@ -14,7 +14,6 @@ def get_ontology_version(path, version=None):
         raise Http404
 
     versions = os.listdir(path)
-    versions.remove(".DS_Store")
     if not version:
         version = max(
             (d for d in versions), key=lambda d: [int(x) for x in d.split(".")]
