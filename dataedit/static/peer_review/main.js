0aef5e87401868c8b16a62805f20ce3af538dece
Darynarli <Daryna.Barabanova@rl-institut.de>
2025-07-31 18:14:37 +0200

    connect navigation functions to main.js #2072

--- dataedit/static/peer_review/main.js
+++ dataedit/static/peer_review/main.js
@@ -1,7 +1,14 @@
 // SPDX-FileCopyrightText: 2025 Reiner Lemoine Institut
 // SPDX-License-Identifier: AGPL-3.0-or-later
 import * as common from "./peer_review.js";
+import { selectState } from './peer_review.js';
+window.selectState = selectState;
 
+import { selectNextField } from './navigation.js'
+window.selectNextField = selectNextField;
+
+import { selectPreviousField } from './navigation.js'
+window.selectPreviousField = selectPreviousField;
 document.addEventListener('DOMContentLoaded', function () {
     common.initCurrentReview(config);
   common.peerReview(config, true);
