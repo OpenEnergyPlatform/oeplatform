d657a9f9251abb40790cabcae6d43146222688c4
jh-RLI <jonas.huber@rl-institut.de>
2025-09-08 14:29:46 +0200

    #2069 #2087 SB: Adapt related output input dataset filters to OEKG v2

--- factsheet/oekg/filters.py
+++ factsheet/oekg/filters.py
@@ -58,23 +58,21 @@ class OekgQuery:
         related_scenarios = set()
         table_iri = self.serialize_table_iri(table_iri)
 
-        # Find all scenario bundles
-        for s, p, o in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
-            # find all scenarios in any bundle
-            for s1, p1, o1 in self.oekg.triples((s, namespaces.OEO.BFO_0000051, None)):
-                # Find scenarios where the given table is the input dataset
-                for s2, p2, o1_input_ds_uid in self.oekg.triples(
-                    (o1, namespaces.OEO.OEO_00020437, None)
+        for s, _, _ in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
+            # Correct BFO namespace here:
+            for _, _, part in self.oekg.triples((s, namespaces.OBO.BFO_0000051, None)):
+                # Only scenarios have inputs; cheap pre-check (optional)
+                # if (part, RDF.type, namespaces.OEO.OEO_00000365) not in self.oekg:
+                #     continue
+                for subj, _, ds in self.oekg.triples(
+                    (part, namespaces.OEO.OEO_00020437, None)
                 ):
-                    if o1_input_ds_uid is not None:
-                        for s3, p3, o3_input_ds_iri in self.oekg.triples(
-                            (o1_input_ds_uid, namespaces.OEO.OEO_00390094, None)
-                        ):
-                            if (
-                                self.serialize_table_iri(str(o3_input_ds_iri))
-                                == table_iri
-                            ):
-                                related_scenarios.add(s2)
+                    # ds is the dataset node; compare its IRI/url property
+                    for _, _, ds_iri in self.oekg.triples(
+                        (ds, namespaces.OEO.OEO_00390094, None)
+                    ):
+                        if self.serialize_table_iri(str(ds_iri)) == table_iri:
+                            related_scenarios.add(subj)
 
         return related_scenarios
 
@@ -95,25 +93,18 @@ class OekgQuery:
         table_iri = self.serialize_table_iri(table_iri)
 
         # Find all scenario bundles
-        for s, p, o in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
-            # find all scenarios in any bundle
-            for s1, p1, o1 in self.oekg.triples((s, namespaces.OEO.BFO_0000051, None)):
-                for s2, p2, o2_output_ds_uid in self.oekg.triples(
-                    (o1, namespaces.OEO.OEO_00020436, None)
+        for s, _, _ in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
+            for _, _, part in self.oekg.triples(
+                (s, namespaces.OBO.BFO_0000051, None)
+            ):  # FIX
+                for subj, _, ds in self.oekg.triples(
+                    (part, namespaces.OEO.OEO_00020436, None)
                 ):
-                    if o2_output_ds_uid is not None:
-                        for s3, p3, o3_output_ds_iri in oekg.triples(
-                            (
-                                o2_output_ds_uid,
-                                namespaces.OEO.OEO_00390094,
-                                None,
-                            )
-                        ):
-                            if (
-                                self.serialize_table_iri(str(o3_output_ds_iri))
-                                == table_iri
-                            ):
-                                related_scenarios.add(s2)
+                    for _, _, ds_iri in self.oekg.triples(
+                        (ds, namespaces.OEO.OEO_00390094, None)
+                    ):
+                        if self.serialize_table_iri(str(ds_iri)) == table_iri:
+                            related_scenarios.add(subj)
 
         return related_scenarios
 
@@ -146,11 +137,15 @@ class OekgQuery:
 
         scenario_bundles_input = set()
 
-        for s, p, o in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
-            for i in related_scenarios_input:
-                for s1, p1, o1 in oekg.triples((s, namespaces.OEO.BFO_0000051, i)):
+        for bundle, _, _ in self.oekg.triples(
+            (None, RDF.type, namespaces.OEO.OEO_00020227)
+        ):
+            for scen in related_scenarios_input:
+                for s1, _, _ in self.oekg.triples(
+                    (bundle, namespaces.OBO.BFO_0000051, scen)
+                ):
                     if s1:
-                        scenario_bundles_input.add((s1, s))
+                        scenario_bundles_input.add((s1, bundle))
 
         return scenario_bundles_input
 
@@ -175,11 +170,15 @@ class OekgQuery:
         )
         scenario_bundles_output = set()
 
-        for s, p, o in self.oekg.triples((None, RDF.type, namespaces.OEO.OEO_00020227)):
-            for i in related_scenarios_output:
-                for s1, p1, o1 in oekg.triples((s, namespaces.OEO.BFO_0000051, i)):
+        for bundle, _, _ in self.oekg.triples(
+            (None, RDF.type, namespaces.OEO.OEO_00020227)
+        ):
+            for scen in related_scenarios_output:
+                for s1, _, _ in self.oekg.triples(
+                    (bundle, namespaces.OBO.BFO_0000051, scen)
+                ):
                     if s1:
-                        scenario_bundles_output.add((s1, s))
+                        scenario_bundles_output.add((s1, bundle))
 
         return scenario_bundles_output
 
