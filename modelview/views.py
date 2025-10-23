10e3cb7971ae54c982c7c33d6c28d4ed2d7a7bba
Christian Winger <c@wingechr.de>
2025-10-23 22:13:51 +0200

    factsheets fix tag handling

--- modelview/views.py
+++ modelview/views.py
@@ -24,7 +24,6 @@ import urllib3
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.postgres.fields import ArrayField
-from django.db.models import Q
 from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
