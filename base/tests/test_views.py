046be990f444b691e740507b88b9f82b622d7539
Christian Winger <c.winger@oeko.de>
2025-10-29 08:07:27 +0100

    fixurls in forms

--- base/tests/test_views.py
+++ base/tests/test_views.py
@@ -4,7 +4,6 @@ SPDX-FileCopyrightText: 2025 Martin Glauer <https://github.com/MGlauer> © Otto-
 SPDX-License-Identifier: AGPL-3.0-or-later
 """  # noqa: 501
 
-import logging
 import re
 from pathlib import Path
 
@@ -33,7 +32,6 @@ class TestViewsBase(TestViewsTestCase):
     def test_urls_in_templates(self):
         resolvers = {}
         root_dir = Path(BASE_DIR)
-        errors = []
 
         EXTERNAL_URLS_REV = {v: k for k, v in EXTERNAL_URLS.items()}
 
@@ -44,7 +42,14 @@ class TestViewsBase(TestViewsTestCase):
             return x
 
         def can_resolve_name(name: str) -> bool:
-            app = name.split(":")[0] if ":" in name else name
+            if ":" in name:
+                app = name.split(":")[0]
+            elif name.startswith("account_"):
+                app = "allauth"  # allauth does not use prefix, apparently
+                name = f"{app}:{name}"
+            else:
+                app = None
+
             if app not in resolvers:
                 try:
                     resolvers[app] = get_app_reverse_lookup_names_and_kwargs(app)
@@ -52,55 +57,49 @@ class TestViewsBase(TestViewsTestCase):
                     return False
             return name in resolvers[app]
 
+        def find_url_problem(url: str, location: str) -> str | None:
+            # get string between quotes
+            value = unquote(url)
+            if not value:
+                return None
+            elif re.match(r"^({%|#|\?|{{|mailto:|javascript:)", value):
+                # find reverse url pattern
+                match_url = re.match(".*{% url[ ]+(.*)%}", value)
+                if match_url:
+                    # get string between quotes
+                    url_value = match_url.groups()[0].strip()
+                    # get only first part (name)
+                    reverse_name = unquote(url_value.split(" ")[0])
+                    if not can_resolve_name(reverse_name):
+                        return f"Cannot reverse url: {url_value} in {location}"
+            elif re.match("^(http:|https:|//)", value):
+                # should be external link
+                if value in EXTERNAL_URLS_REV:
+                    return f"Use existing EXTERNAL_URLS.{EXTERNAL_URLS_REV[value]}: {value} in {location}"  # noqa: 501
+                else:
+                    return f"Use (create new) EXTERNAL_URLS: {value} in {location}"
+            elif re.match("^/static/", value):
+                # should be static link
+                return f"Use %static: {value} in {location}"
+            else:
+                # other (should probably be reverse lookup)
+                return f"Use %url: {value} in {location}"
+
+        errors = []
+
         # find all templates
         for path in root_dir.glob("*/templates/**/*.html"):
             with open(path, encoding="utf-8") as f:
                 for idx, line in enumerate(f):
-                    location = f"{path}:{idx+1}"
                     # find href and src tags
                     for match in re.findall(
-                        r"(src|href)[ ]*=[ ]*('([^']+)'|\"([^\"]+)\")", line
+                        r"(src|href|action)[ ]*=[ ]*('([^']+)'|\"([^\"]+)\")", line
                     ):
-                        # get string between quotes
-                        value = unquote(match[1])
-                        if not value:
-                            continue
-                        elif re.match(r"^({%|#|\?|{{|mailto:|javascript:)", value):
-                            # find reverse url pattern
-                            match_url = re.match(".*{% url[ ]+(.*)%}", value)
-                            if match_url:
-                                # get string between quotes
-                                url_value = match_url.groups()[0].strip()
-                                # get only first part (name)
-                                reverse_name = unquote(url_value.split(" ")[0])
-                                if not can_resolve_name(reverse_name):
-                                    # errors.append(
-                                    #    (location, "Cannot reverse url", url_value)
-                                    # )
-                                    pass
-                        elif re.match("^(http:|https:|//)", value):
-                            # should be external link
-                            if value in EXTERNAL_URLS_REV:
-                                errors.append(
-                                    (
-                                        location,
-                                        f"Use existing EXTERNAL_URLS.{EXTERNAL_URLS_REV[value]}",  # noqa:501
-                                        value,
-                                    )
-                                )
-                            else:
-                                errors.append(
-                                    (location, "Use (create new) EXTERNAL_URLS", value)
-                                )
-                        elif re.match("^/static/", value):
-                            # should be static link
-                            errors.append((location, "Use {% static %}", value))
-                        else:
-                            # other (should probably be reverse lookup)
-                            errors.append((location, "Use {% url %}", value))
-
+                        error = find_url_problem(
+                            url=match[1], location=f"{path}:{idx+1}"
+                        )
+                        if error:
+                            errors.append(error)
         if errors:
-            errors = [" ".join(e) for e in errors]
             error_txt = "Errors in urls in templates:\n\n" + "\n".join(errors)
-            # raise Exception(error_txt)
-            logging.error(error_txt)
+            raise Exception(error_txt)
