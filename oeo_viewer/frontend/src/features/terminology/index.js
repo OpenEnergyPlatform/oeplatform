f70b2d23dd421a89cbbed0389f47267fcf227701
jh-RLI <jonas.huber@rl-institut.de>
2026-02-12 14:22:47 +0100

    #1911: remove deprecated file

--- oeo_viewer/frontend/src/features/terminology/index.js
+++
@@ -1,4 +0,0 @@
-// SPDX-FileCopyrightText: 2025 Jonas Huber <https://github.com/jh-RLI> © Reiner Lemoine Institut
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-export { TssConfigProvider } from "./config/TssConfigProvider";
