b67d1be541424adc87bebe91839f0945764b4cb8
vismayajochem <vismayajochem>
2026-04-23 10:35:26 +0200

    Add authors #2299

--- dataedit/static/peer_review/main.js
+++ dataedit/static/peer_review/main.js
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: 2025 Reiner Lemoine Institut
+// SPDX-FileCopyrightText: 2025 Daryna Barabanova <https://github.com/Darynarli> © Reiner Lemoine Institut
+// SPDX-FileCopyrightText: 2026 Jonas Huber <https://github.com/jh-RLI> © Reiner Lemoine Institut
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import * as common from "./peer_review.js";
