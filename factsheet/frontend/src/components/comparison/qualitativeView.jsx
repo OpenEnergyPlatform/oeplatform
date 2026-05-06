1bae4785ce154caeb8c1fdf6e66b6e814ece0109
jh-RLI <jonas.huber@rl-institut.de>
2026-05-06 11:03:09 +0200

    #2188 add spdx header

--- factsheet/frontend/src/components/comparison/qualitativeView.jsx
+++ factsheet/frontend/src/components/comparison/qualitativeView.jsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Jonas Huber <https://github.com/jh-RLI> © Reiner Lemoine Institut
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import FormGroup from "@mui/material/FormGroup";
