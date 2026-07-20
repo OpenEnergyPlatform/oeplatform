c8046d39336b53ee1372894b4cba77ed47ee7e63
vismayajochem <vismayajochem>
2026-07-20 11:50:48 +0200

    First try of implementing, not working #9

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -3,7 +3,11 @@ SPDX-FileCopyrightText: 2025 Martin Glauer <https://github.com/MGlauer> © Otto-
 SPDX-License-Identifier: AGPL-3.0-or-later
 """  # noqa: 501
 
+import os
+
+import markdown2
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -11,3 +15,17 @@ register = template.Library()
 @register.filter(name="addclass")
 def addclass(field, css):
     return field.as_widget(attrs={"class": css})
+
+
+@register.simple_tag
+def render_markdown(filename):
+    """Read a markdown file from static/content/ and render it as HTML."""
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    md_path = os.path.join(base_dir, "static", "content", filename)
+    try:
+        with open(md_path, encoding="utf-8") as f:
+            content = f.read()
+        markdowner = markdown2.Markdown()
+        return mark_safe(markdowner.convert(content))
+    except FileNotFoundError:
+        return ""
